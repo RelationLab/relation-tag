d8f5ab54df1a42c7f6701466aa36cc1e3c08d1a5
panzuchao <panzuchao@126.com>
2022-10-24 19:25:07 +0800

    update config

--- app/tag-app/src/main/resources/initsql/address_labels_json_gin.sql
+++ app/tag-app/src/main/resources/initsql/address_labels_json_gin.sql
@@ -1,3 +1,14 @@
+drop table if exists address_label_gp;
+CREATE TABLE public.address_label_gp
+(
+    "owner"    varchar(256) NULL,
+    address    varchar(512) NULL,
+    label_type varchar(512) NULL,
+    label_name varchar(1024) NULL,
+    "source"   varchar(100) NULL,
+    updated_at timestamp(6) NULL
+) DISTRIBUTED BY (address);
+
 drop table if exists address_labels_json_gin;
 create table address_labels_json_gin
 (
