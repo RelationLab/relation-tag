0b9e3d8cdcd4357a763f72e49f670466ce6d6c8a
panzuchao <panzuchao@126.com>
2022-10-26 14:13:59 +0800

    sql

--- app/tag-app/src/main/java/com/relation/tag/manager/impl/TagAddressManagerImpl.java
+++ app/tag-app/src/main/java/com/relation/tag/manager/impl/TagAddressManagerImpl.java
@@ -99,7 +99,6 @@ public class TagAddressManagerImpl implements TagAddressManager {
         iAddressLabelService.exceSql(FileUtils.readFile(FILEPATH.concat(File.separator).concat("dim_rule_content.sql")),"dim_rule_content.sql");
         iAddressLabelService.exceSql(FileUtils.readFile(FILEPATH.concat(File.separator).concat("nft_volume_count.sql")),"nft_volume_count.sql");
         iAddressLabelService.exceSql(FileUtils.readFile(FILEPATH.concat(File.separator).concat("platform_nft_type_volume_count.sql")),"platform_nft_type_volume_count.sql");
-
         iAddressLabelService.exceSql(FileUtils.readFile(FILEPATH.concat(File.separator).concat("dim_rank_token.sql")), "dim_rank_token.sql");
         iAddressLabelService.exceSql(FileUtils.readFile(FILEPATH.concat(File.separator).concat("dim_rule_sql_content.sql")), "dim_rule_sql_content.sql");
         iAddressLabelService.exceSql(FileUtils.readFile(FILEPATH.concat(File.separator).concat("address_labels_json_gin.sql")), "address_labels_json_gin.sql");
