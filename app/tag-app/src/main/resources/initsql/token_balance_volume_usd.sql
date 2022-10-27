14ec4a3887fbf08be86e35fe2f901fd7cff60160
panzuchao <panzuchao@126.com>
2022-10-27 21:50:44 +0800

    auto sql scripts

--- app/tag-app/src/main/resources/initsql/token_balance_volume_usd.sql
+++ app/tag-app/src/main/resources/initsql/token_balance_volume_usd.sql
@@ -13,7 +13,6 @@ create table public.token_balance_volume_usd
 insert into token_balance_volume_usd(address, token, balance_usd, volume_usd)
     select eh.address as address, 'eth' as token, eh.balance * price asbalance_usd , eh.total_transfer_all_volume* price as volume_usd from eth_holding eh
      inner join white_list_erc20 wle on symbol='WETH' where eh.balance > 0.04 or eh.total_transfer_all_volume>0;
-
 insert into token_balance_volume_usd(address, token, balance_usd, volume_usd)
     (select th.address, token, th.balance * price, total_transfer_all_volume* price from token_holding th
     inner join white_list_erc20 wle on th.token = wle.address and ignored = false where
