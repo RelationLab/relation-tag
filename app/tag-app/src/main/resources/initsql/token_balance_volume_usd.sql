50076e382feeb737e4cee748b7ae34c20866fa9b
panzuchao <panzuchao@126.com>
2022-11-01 19:10:14 +0800

    delete token balace>0

--- app/tag-app/src/main/resources/initsql/token_balance_volume_usd.sql
+++ app/tag-app/src/main/resources/initsql/token_balance_volume_usd.sql
@@ -12,8 +12,7 @@ create table public.token_balance_volume_usd
 
 insert into token_balance_volume_usd(address, token, balance_usd, volume_usd)
     select distinct eh.address as address, 'eth' as token, eh.balance * price asbalance_usd , eh.total_transfer_all_volume* price as volume_usd from eth_holding eh
-     inner join white_list_erc20 wle on symbol='WETH' where eh.balance > 0.04 or eh.total_transfer_all_volume>0;
+     inner join white_list_erc20 wle on symbol='WETH';
 insert into token_balance_volume_usd(address, token, balance_usd, volume_usd)
-    (select distinct th.address, token, th.balance * price as balance_usd, total_transfer_all_volume* price as volume_usd from token_holding th
-    inner join white_list_erc20 wle on th.token = wle.address and ignored = false where
-   (th.balance > 0 or th.total_transfer_all_volume>0));
+    select distinct th.address, token, th.balance * price as balance_usd, total_transfer_all_volume* price as volume_usd from token_holding th
+    inner join white_list_erc20 wle on th.token = wle.address and ignored = false;
