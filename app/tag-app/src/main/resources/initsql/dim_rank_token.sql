b2ee6c9455c9788b4ff14a94a13d56f2e956819f
panzuchao <panzuchao@126.com>
2022-11-09 11:52:44 +0800

    add total_balance_volume_usd scripts

--- app/tag-app/src/main/resources/initsql/dim_rank_token.sql
+++ app/tag-app/src/main/resources/initsql/dim_rank_token.sql
@@ -1,5 +1,20 @@
 drop table if exists dim_rank_token;
-CREATE TABLE public.dim_rank_token (
-    token_id varchar(512) NULL
+create table dim_rank_token
+(
+    token_id   varchar(512),
+    asset_type varchar(10)
 );
-insert into dim_rank_token select distinct token from dim_rule_content;
+insert into dim_rank_token
+select distinct token,token_type from dim_rule_content;
+
+
+
+
+
+
+
+
+
+
+
+
