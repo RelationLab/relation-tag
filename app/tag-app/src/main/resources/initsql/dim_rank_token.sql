97de1c5f4d696b753012d1de7ae0d7f4e9edb536
panzuchao <panzuchao@126.com>
2022-11-10 13:02:50 +0800

    delete distributed

--- app/tag-app/src/main/resources/initsql/dim_rank_token.sql
+++ app/tag-app/src/main/resources/initsql/dim_rank_token.sql
@@ -4,8 +4,7 @@ create table dim_rank_token
     token_id   varchar(512),
     asset_type varchar(10)
 );
-insert into dim_rank_token
-select distinct token,token_type from dim_rule_content;
+insert into dim_rank_token select distinct token,token_type from dim_rule_content;
 
 
 
