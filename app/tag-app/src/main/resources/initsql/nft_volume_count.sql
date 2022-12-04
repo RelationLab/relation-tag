45170c62a4dc988d4cd83b5d0413180752742a06
HP <panzuchao@126.com>
2022-12-04 18:21:21 +0800

    add univ3 scripts

--- app/tag-app/src/main/resources/initsql/nft_volume_count.sql
+++ app/tag-app/src/main/resources/initsql/nft_volume_count.sql
@@ -27,7 +27,7 @@ insert into nft_volume_count(address, token, type, transfer_volume, transfer_cou
     select address, token , 'Sale', total_transfer_sell_volume, total_transfer_sell_count from nft_buy_sell_holding;
 
 insert into nft_volume_count(address, token, type, transfer_volume, transfer_count)
-    select address, token , 'ALL', total_transfer_all_volume, total_transfer_all_count from nft_holding;
+    select address, token , 'ALL', total_transfer_all_volume, total_transfer_count from nft_holding;
 
 insert into nft_volume_count(address, token, type, transfer_volume, transfer_count)
     select address, token , 'Transfer', total_transfer_volume, total_transfer_count from nft_transfer_holding;
