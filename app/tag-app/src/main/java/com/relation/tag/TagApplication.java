4b9a9d7d2fb21beb7cda4948ebd5967961284c0c
HP <panzuchao@126.com>
2022-12-26 23:19:09 +0800

    add univ3 scripts

--- app/tag-app/src/main/java/com/relation/tag/TagApplication.java
+++ app/tag-app/src/main/java/com/relation/tag/TagApplication.java
@@ -1,21 +1,27 @@
 package com.relation.tag;
 
 import com.relation.tag.manager.TagAddressManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication(scanBasePackages = {"com.relation.tag",
         "org.springframework.boot.extension"})
 @EnableScheduling
 public class TagApplication {
-
-    public static void main(String[] args) throws Exception {
+    @Autowired
+    private TagAddressManager tagAddressManager;
+    public static void main(String[] args)  {
         ConfigurableApplicationContext ctx =  SpringApplication.run(TagApplication.class, args);
-        TagAddressManager tagAddressManager = ctx.getBean(TagAddressManager.class);
-        tagAddressManager.refreshAllLabel();
     }
-
+    @PostConstruct
+    public void postConstruct() throws Exception {
+        tagAddressManager.refreshAllLabel();
+        System.out.println("执行Springboot正式启动前的代码");
+    }
 }
