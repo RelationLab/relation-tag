48b210f27843eaccbbfa59580aaaea37d6d3c2a6
HP <panzuchao@126.com>
2022-12-27 12:23:35 +0800

    exit 0

--- app/tag-app/src/main/java/com/relation/tag/manager/impl/TagAddressManagerImpl.java
+++ app/tag-app/src/main/java/com/relation/tag/manager/impl/TagAddressManagerImpl.java
@@ -82,16 +82,16 @@ public class TagAddressManagerImpl implements TagAddressManager {
     @Override
     public void refreshAllLabel() throws Exception {
         innit();
-//        List<DimRuleSqlContent> ruleSqlList = dimRuleSqlContentService.list();
-//        List<FileEntity> fileList = Lists.newArrayList();
-//        for (DimRuleSqlContent item : ruleSqlList) {
-//            String fileName = item.getRuleName().concat(".sql");
-//            fileList.add(FileEntity.builder().fileName(fileName)
-//                    .fileContent(FileUtils.readFile(SCRIPTSPATH.concat(File.separator)
-//                            .concat(fileName))).build());
-//        }
-//        check("web3_transaction_record_summary", 60 * 1000);
-//        tagByRuleSqlList(fileList, false);
+        List<DimRuleSqlContent> ruleSqlList = dimRuleSqlContentService.list();
+        List<FileEntity> fileList = Lists.newArrayList();
+        for (DimRuleSqlContent item : ruleSqlList) {
+            String fileName = item.getRuleName().concat(".sql");
+            fileList.add(FileEntity.builder().fileName(fileName)
+                    .fileContent(FileUtils.readFile(SCRIPTSPATH.concat(File.separator)
+                            .concat(fileName))).build());
+        }
+        check("web3_transaction_record_summary", 60 * 1000);
+        tagByRuleSqlList(fileList, false);
     }
 
     private void innit() throws Exception {
@@ -101,15 +101,15 @@ public class TagAddressManagerImpl implements TagAddressManager {
         iAddressLabelService.exceSql(FileUtils.readFile(FILEPATH.concat(File.separator).concat("dim_rule_content.sql")), "dim_rule_content.sql");
         iAddressLabelService.exceSql(FileUtils.readFile(FILEPATH.concat(File.separator).concat("dim_rule_sql_content.sql")), "dim_rule_sql_content.sql");
         iAddressLabelService.exceSql(FileUtils.readFile(FILEPATH.concat(File.separator).concat("dim_rank_token.sql")), "dim_rank_token.sql");
-//        execSql("dim_rank_token", "platform_nft_volume_usd.sql");
-//        execSql("platform_nft_volume_usd", "nft_transfer_holding.sql");
-//        execSql("nft_transfer_holding", "nft_volume_count.sql");
-//        execSql("nft_volume_count", "platform_nft_type_volume_count.sql");
-//        execSql("platform_nft_type_volume_count", "dex_tx_volume_count_summary.sql");
-//        execSql("dex_tx_volume_count_summary", "dex_tx_volume_count_summary_univ3.sql");
-//        execSql("dex_tx_volume_count_summary", "token_balance_volume_usd.sql");
-//        execSql("token_balance_volume_usd", "total_balance_volume_usd.sql");
-//        execSql("total_balance_volume_usd", "web3_transaction_record_summary.sql");
+        execSql("dim_rank_token", "platform_nft_volume_usd.sql");
+        execSql("platform_nft_volume_usd", "nft_transfer_holding.sql");
+        execSql("nft_transfer_holding", "nft_volume_count.sql");
+        execSql("nft_volume_count", "platform_nft_type_volume_count.sql");
+        execSql("platform_nft_type_volume_count", "dex_tx_volume_count_summary.sql");
+        execSql("dex_tx_volume_count_summary", "dex_tx_volume_count_summary_univ3.sql");
+        execSql("dex_tx_volume_count_summary", "token_balance_volume_usd.sql");
+        execSql("token_balance_volume_usd", "total_balance_volume_usd.sql");
+        execSql("total_balance_volume_usd", "web3_transaction_record_summary.sql");
     }
 
     private void execSql(String lastTableName, String sqlName) {
