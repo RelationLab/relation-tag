b76419df1fdc268f2fe6aa06c83d8a93eafdeb4d
HP <panzuchao@126.com>
2023-01-08 19:40:51 +0800

    add univ3 scripts

--- app/tag-app/src/main/resources/initsql/total_balance_volume_usd.sql
+++ app/tag-app/src/main/resources/initsql/total_balance_volume_usd.sql
@@ -1,4 +1,4 @@
 
 
 insert into total_balance_volume_usd(address, balance_usd, volume_usd)
-     (select address, sum(balance_usd), sum(volume_usd) from token_balance_volume_usd group by address);
+     (select address, sum(balance_usd), sum(volume_usd) from token_balance_volume_usd where address is not null group by address);
