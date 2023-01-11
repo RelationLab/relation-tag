a01f932333f1df7bea5ea3889cc60df33a113cf0
HP <panzuchao@126.com>
2023-01-11 23:16:49 +0800

    add univ3 scripts

--- app/tag-app/src/main/java/com/relation/tag/manager/impl/TagAddressManagerImpl.java
+++ app/tag-app/src/main/java/com/relation/tag/manager/impl/TagAddressManagerImpl.java
@@ -43,7 +43,7 @@ public class TagAddressManagerImpl implements TagAddressManager {
             throw new RuntimeException(e);
         }
         try {
-            Thread.sleep(1 * 60 * 1000);
+            Thread.sleep(90 * 60 * 1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
