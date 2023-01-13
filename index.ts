9f08b389d7c2b33b20c1eb7b72c9da1c6f24af75
matrix <113445886@qq.com>
2023-01-13 08:30:40 +0800

    feat: change to job & use upstream ecr

--- index.ts
+++ index.ts
@@ -2,77 +2,23 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
 const name = "wired-tag";
-const stack = pulumi.getStack();
+const config = new pulumi.Config();
+const maintainer = config.require("maintainer");
 
-const tags = {
-    Name: `${name}-${stack}`,
-    Project: "Wired.network",
-    Stack: `Pulumi-${stack}`,
-};
-
-const ecr = new aws.ecr.Repository(name, {
-    name,
-    tags
-});
-
-new aws.ecr.RepositoryPolicy(
-    tags.Name,
-    {
-      repository: ecr.id,
-      policy: JSON.stringify({
-        Version: "2012-10-17",
-        Statement: [
-          {
-            Sid: "new policy",
-            Effect: "Allow",
-            Principal: "*",
-            Action: [
-              "ecr:GetDownloadUrlForLayer",
-              "ecr:BatchGetImage",
-              "ecr:BatchCheckLayerAvailability",
-              "ecr:PutImage",
-              "ecr:InitiateLayerUpload",
-              "ecr:UploadLayerPart",
-              "ecr:CompleteLayerUpload",
-              "ecr:DescribeRepositories",
-              "ecr:GetRepositoryPolicy",
-              "ecr:ListImages",
-              "ecr:DeleteRepository",
-              "ecr:BatchDeleteImage",
-              "ecr:SetRepositoryPolicy",
-              "ecr:DeleteRepositoryPolicy",
-            ],
-          },
-        ],
-      }),
-    },
-    { deleteBeforeReplace: true }
+const wiredAwsEcrStackRef = aws
+  .getRegionOutput()
+  .apply(
+    (res) =>
+      new pulumi.StackReference(`${maintainer}/wired-aws-ecr/${res.name}`)
   );
 
-  new aws.ecr.LifecyclePolicy(
-    tags.Name,
-    {
-      repository: ecr.id,
-      policy: JSON.stringify({
-        rules: [
-          {
-            rulePriority: 1,
-            description: "Expire images older than 14 days",
-            selection: {
-              tagStatus: "untagged",
-              countType: "sinceImagePushed",
-              countUnit: "days",
-              countNumber: 14,
-            },
-            action: {
-              type: "expire",
-            },
-          },
-        ],
-      }),
-    },
-    { deleteBeforeReplace: true }
-);
+const wiredAutoTagStackRefOutputs = new pulumi
+  .StackReference(`${maintainer}/wired-auto-tag/prod`)
+  .getOutput("outputs")
 
-export const ecrRepositoryName = ecr.name;
-export const ecrRepositoryUrl = ecr.repositoryUrl;
+export const ecrRepositoryName = name;
+export const ecrRepositoryUrl = wiredAwsEcrStackRef.apply((stack) =>
+  stack.getOutput("ecrRepositories").apply((repos) => repos[name])
+);
+export const wiredAutoTagAwsSecretId = wiredAutoTagStackRefOutputs.apply((outputs) => outputs["awsSecretId"]);
+export const wiredAutoTagGreenplumHost = wiredAutoTagStackRefOutputs.apply((outputs) => outputs["gpHost"]);
