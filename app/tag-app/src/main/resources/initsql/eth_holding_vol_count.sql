a46d12a1d4bb97bb64f545e943b46479d98b211d
HP <panzuchao@126.com>
2023-02-14 04:28:55 +0800

    add univ3 scripts

--- app/tag-app/src/main/resources/initsql/eth_holding_vol_count.sql
+++ app/tag-app/src/main/resources/initsql/eth_holding_vol_count.sql
@@ -1,6 +1,7 @@
 create table eth_holding_vol_count_tmp as select * from eth_holding_vol_count;
 truncate table eth_holding_vol_count;
-insert into
+insert
+into
     eth_holding_vol_count(address,
                           total_transfer_volume,
                           total_transfer_count,
@@ -19,17 +20,18 @@ select
 from
     (
         select
-             address,
+            address,
             sum(total_transfer_volume) total_transfer_volume,
             sum(total_transfer_count) total_transfer_count,
-             sum(total_transfer_to_count) as total_transfer_to_count,
+            sum(total_transfer_to_count)  total_transfer_to_count,
             sum(total_transfer_all_count) total_transfer_all_count,
-             sum(total_transfer_to_volume) as total_transfer_to_volume,
-            sum(total_transfer_all_volume) total_transfer_all_volume
+            sum(total_transfer_to_volume)  total_transfer_to_volume,
+            sum(total_transfer_all_volume)  total_transfer_all_volume
         from
             eth_holding_vol_count_tmp
         group by
             address
+        union  all
         select
             from_address address,
             sum(amount) total_transfer_volume,
@@ -39,22 +41,28 @@ from
             0 as total_transfer_to_volume,
             sum(amount) total_transfer_all_volume
         from
-            eth_tx_record where amount>0
+            eth_tx_record
+        where
+                amount>0
         group by
             from_address
-
-        union all select
-                      to_address address,
-                      0 as total_transfer_volume,
-                      0 as total_transfer_count,
-                      sum(1) as total_transfer_to_count,
-                      sum(1) total_transfer_all_count,
-                      sum(amount) as total_transfer_to_volume,
-                      sum(amount) total_transfer_all_volume
+        union all
+        select
+            to_address address,
+            0 as total_transfer_volume,
+            0 as total_transfer_count,
+            sum(1) as total_transfer_to_count,
+            sum(1) total_transfer_all_count,
+            sum(amount) as total_transfer_to_volume,
+            sum(amount) total_transfer_all_volume
         from
-            eth_tx_record where amount>0
+            eth_tx_record
+        where
+                amount>0
         group by
-            to_address) atb group by  address;
+            to_address) atb
+group by
+    address;
 insert into dms_syn_block(syn_type,block_height)
-    select 'eth_tx_record' as syn_type,max(block_number) from eth_tx_record;
+select 'eth_tx_record' as syn_type,max(block_number) from eth_tx_record;
 
