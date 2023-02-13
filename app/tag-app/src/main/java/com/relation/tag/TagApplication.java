8b674be6361180adeefc20ea6a4fee8480d6807b
HP <panzuchao@126.com>
2023-02-14 04:31:40 +0800

    add univ3 scripts

--- app/tag-app/src/main/java/com/relation/tag/TagApplication.java
+++ app/tag-app/src/main/java/com/relation/tag/TagApplication.java
@@ -13,25 +13,25 @@ public class TagApplication {
 
     public static void main(String[] args) throws Exception {
         ConfigurableApplicationContext ctx = SpringApplication.run(TagApplication.class, args);
-//        TagAddressManager tagAddressManager = ctx.getBean(TagAddressManager.class);
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                try {
-//                    tagAddressManager.refreshAllLabel();
-//                } catch (Exception e) {
-//                    throw new RuntimeException(e);
-//                }
-//            }
-//        }).start();
-//        if (tagAddressManager.checkResult("address_labels_json_gin")){
-//            log.info("checkResult tag end...........");
-//            System.exit(0);
-//        }
-//        Thread.sleep(240*60*1000);
-//        log.info("check address_labels_json_gin start...........");
-//        tagAddressManager.check("address_labels_json_gin", 1 * 60 * 1000);
-//        log.info("tag end...........");
+        TagAddressManager tagAddressManager = ctx.getBean(TagAddressManager.class);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    tagAddressManager.refreshAllLabel();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+        if (tagAddressManager.checkResult("address_labels_json_gin")){
+            log.info("checkResult tag end...........");
+            System.exit(0);
+        }
+        Thread.sleep(240*60*1000);
+        log.info("check address_labels_json_gin start...........");
+        tagAddressManager.check("address_labels_json_gin", 1 * 60 * 1000);
+        log.info("tag end...........");
 //        System.exit(0);
     }
 }
