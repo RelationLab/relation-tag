8bf850ab45276df2678c93b4cad8867537576b49
HP <panzuchao@126.com>
2023-02-24 14:21:55 +0800

    add asset project days and address_type

--- app/tag-app/src/main/resources/initsql/create_tabel.sql
+++ app/tag-app/src/main/resources/initsql/create_tabel.sql
@@ -135,9 +135,39 @@ CREATE TABLE public.dim_rule_sql_content (
 -- Drop table
 
 -- DROP TABLE public.white_list_erc20;
+DROP TABLE IF EXISTS public.white_list_erc20_temp;
+CREATE TABLE public.white_list_erc20_temp as
+select * from white_list_erc20;
+update
+    white_list_erc20 b
+set
+    price = A.price
+    from
+	(
+	select
+		wa.symbol,
+		wa.price
+	from
+		white_list_erc20_temp wa
+	inner join(
+		select
+			max(updated_at) as updated_at,
+			wlet.symbol
+		from
+			white_list_erc20_temp wlet
+		group by
+			wlet.symbol) wb on
+		(wa.symbol = wb.symbol
+			and wa.updated_at = wb.updated_at)) A
+where
+    b.symbol = A.symbol;
+
+
 DROP TABLE IF EXISTS public.white_list_erc20_tag;
 CREATE TABLE public.white_list_erc20_tag as
   select * from white_list_erc20;
+
+
 
 
 
