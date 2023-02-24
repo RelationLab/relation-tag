5aecc9dfffdcd7e8b6b7620b5dde85df44954e87
HP <panzuchao@126.com>
2023-02-24 15:32:21 +0800

    add asset project days and address_type

--- app/tag-app/src/main/resources/initsql/platform_nft_volume_usd.sql
+++ app/tag-app/src/main/resources/initsql/platform_nft_volume_usd.sql
@@ -25,7 +25,7 @@ insert into platform_nft_volume_usd(address, platform_group, platform, quote_tok
             total_transfer_all_volume * price,
             total_transfer_to_volume * price,
             total_transfer_volume * price
-     from platform_nft_holding pnh
+     from platform_nft_holding pnh   
               inner join white_list_erc20 w on pnh.quote_token = w.address
      where pnh.token in (select nft_sync_address.address from nft_sync_address) or
          pnh.token='0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb');
