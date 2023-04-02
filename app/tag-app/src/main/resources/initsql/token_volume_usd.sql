53b851c3527be5946f660f21e850cb3976da0978
HP <panzuchao@126.com>
2023-04-02 18:06:47 +0800

    token_volume_usd.sql

--- app/tag-app/src/main/resources/initsql/token_volume_usd.sql
+++ app/tag-app/src/main/resources/initsql/token_volume_usd.sql
@@ -8,7 +8,7 @@ CREATE TABLE public.token_volume_usd (
                                          updated_at timestamp NULL,
                                          removed bool NULL
 )
-distributed by (token);
+distributed by (address);
 truncate table token_volume_usd;
 insert
 into
