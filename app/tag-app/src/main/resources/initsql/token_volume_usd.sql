ab51c184a2e09c99285427d6831de68c3edfad5c
HP <panzuchao@126.com>
2023-04-02 19:13:12 +0800

    token_volume_usd.sql

--- app/tag-app/src/main/resources/initsql/token_volume_usd.sql
+++ app/tag-app/src/main/resources/initsql/token_volume_usd.sql
@@ -10,31 +10,17 @@ CREATE TABLE public.token_volume_usd (
 )
 distributed by (address);
 truncate table token_volume_usd;
-insert
-into
-    token_volume_usd(address,
-                     token,
-                     volume_usd)
-select
-    eh.address as address,
-    'eth' as token,
-    eh.total_transfer_all_volume * round(cast(wle.price as numeric),3)  as volume_usd
-from
-    (select address,sum(total_transfer_all_volume) as total_transfer_all_volume  from eth_holding_vol_count  where total_transfer_all_volume>0
-     group by address
-    ) eh
-        inner join (select price from white_list_erc20 where symbol = 'WETH'  group by price)  wle  on 1=1;
 
 
 insert
 into
     token_volume_usd(address,
-                     token,
-                     volume_usd)
+                             token,
+                             volume_usd)
 select
     th.address,
     token,
-    total_transfer_all_volume * round(cast(wle.price as numeric), 3) as volume_usd
+    round(total_transfer_all_volume * round(cast(wle.price as numeric), 3),3) as volume_usd
 from
     (
         select
@@ -50,10 +36,6 @@ from
                 token_id
             from
                 dim_rank_token)
-        group by
-            address,
-            token,
-            total_transfer_all_volume
     ) th
         inner join (
         select
@@ -68,5 +50,35 @@ from
                     dim_rank_token)) wle on
                 th.token = wle.address
             and ignored = false;
+
+insert
+into
+    token_volume_usd(address,
+                     token,
+                     volume_usd)
+select
+    eh.address as address,
+    'eth' as token,
+    eh.total_transfer_all_volume * round(cast(wle.price as numeric), 3) as volume_usd
+from
+    (
+        select
+            address,
+            total_transfer_all_volume
+        from
+            eth_holding_vol_count
+        where
+                total_transfer_all_volume>0
+    ) eh
+        inner join (
+        select
+            price
+        from
+            white_list_erc20
+        where
+                symbol = 'WETH'
+        group by
+            price) wle on
+            1 = 1;
 
 
