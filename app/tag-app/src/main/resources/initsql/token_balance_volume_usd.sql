0f8b0dfafdb2b32fc861c26e5eb5e49fe0d6ce5b
HP <panzuchao@126.com>
2023-04-10 16:51:29 +0800

    and token in (select token_id from dim_project_token_type_rank dpttr)

--- app/tag-app/src/main/resources/initsql/token_balance_volume_usd.sql
+++ app/tag-app/src/main/resources/initsql/token_balance_volume_usd.sql
@@ -12,7 +12,7 @@ create table public.token_balance_volume_usd
 );
 truncate table token_balance_volume_usd;
 insert into token_balance_volume_usd(address, token, balance_usd, volume_usd)
-select distinct eh.address as address, 'eth' as token, eh.balance * price asbalance_usd , eh.total_transfer_all_volume* price as volume_usd from eth_holding eh
+select distinct eh.address as address, 'eth' as token, eh.balance * price as balance_usd , eh.total_transfer_all_volume* price as volume_usd from eth_holding eh
                                                                                                                                                      inner join white_list_erc20 wle on symbol='WETH' where eh.balance > 0 or eh.total_transfer_all_volume>0;
 insert into token_balance_volume_usd(address, token, balance_usd, volume_usd)
 select distinct th.address, token, th.balance * price as balance_usd, total_transfer_all_volume* price as volume_usd from token_holding th
