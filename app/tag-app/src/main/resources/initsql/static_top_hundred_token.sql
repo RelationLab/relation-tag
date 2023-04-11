d2cd595a183839fbd26f3a7a8ff40e3cca016aec
HP <panzuchao@126.com>
2023-04-11 11:27:16 +0800

    token whale

--- app/tag-app/src/main/resources/initsql/static_top_hundred_token.sql
+++ app/tag-app/src/main/resources/initsql/static_top_hundred_token.sql
@@ -3,28 +3,29 @@ create table static_top_hundred_token
 (
     token  varchar(200) not null,
     rownumber numeric(250, 20) NULL,
+    balance numeric(250, 20) NULL,
     token_name varchar(200) not null,
     token_type varchar(100) not null
 );
 truncate table static_top_hundred_token;
 vacuum static_top_hundred_token;
 
-insert into static_top_hundred_token(token,rownumber,token_name,token_type) values ('ALL',0,'ALL','defi');
-insert into static_top_hundred_token(token,rownumber,token_name,token_type) values ('ALL',0,'ALL','nft');
-
 ----token排名前10
-insert into static_top_hundred_token(token,rownumber,token_name,token_type)
+insert into static_top_hundred_token(token,rownumber,balance,token_name,token_type)
 select distinct s2.token as token,
                 s2.rn as rownumber,
+                balance_usd as balance,
                 drc.token_name as token_name ,
                 'defi' as token_type from (
                                               select
                                                   token,
+                                                  balance_usd,
                                                   rn
                                               from
                                                   (
                                                       select
                                                           token,
+                                                          balance_usd,
                                                           -- 分组字段很关键
                                                           row_number() over( partition by 1=1
 	order by
@@ -40,29 +41,33 @@ select distinct s2.token as token,
                                                                   token)
                                                               rowtable ) s1
                                               where
-                                                      s1.rn <= 10) s2 inner join dim_rule_content drc on(drc.token=s2.token);
+                                                      s1.rn <= 100) s2 inner join dim_rule_content drc on(drc.token=s2.token);
 
 ----nft排名前10
 INSERT
 INTO
     static_top_hundred_token(token,
-                  rownumber,
-                  token_name,
-                  token_type)
+                         rownumber,
+                         balance,
+                         token_name,
+                         token_type)
 SELECT
     distinct s2.token AS token,
              s2.rn AS rownumber,
+             balance,
              drc.token_name AS token_name ,
              'nft' AS token_type
 FROM
     (
         SELECT
             token,
+            balance,
             rn
         FROM
             (
                 SELECT
                     token,
+                    balance,
                     -- 分组字段很关键
                     ROW_NUMBER() OVER( PARTITION BY 1=1
 		ORDER BY
@@ -78,6 +83,6 @@ FROM
                             token)
                         rowtable ) s1
         WHERE
-                s1.rn <= 10) s2
+                s1.rn <= 50) s2
         INNER JOIN dim_project_token_type drc ON
         (drc.token = s2.token);
