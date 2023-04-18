3882de86d75b70387ac0845e5d8d1a6a29224e87
HP <panzuchao@126.com>
2023-04-18 17:20:27 +0800

    WETH 转成eth

--- app/tag-app/src/main/resources/initsql/dex_tx_volume_count_summary.sql
+++ app/tag-app/src/main/resources/initsql/dex_tx_volume_count_summary.sql
@@ -1,5 +1,11 @@
-update dex_tx_volume_count_record_from_gp set project='0x1111111254fb6c44bac0bed2854e76f90643097d'
+update dex_tx_volume_count_record set project='0x1111111254fb6c44bac0bed2854e76f90643097d'
                                           where project='0x1111111254fb6c44bAC0beD2854e76F90643097d';
+
+update dex_tx_volume_count_record set token='eth'
+where (project='0x1111111254fb6c44bAC0beD2854e76F90643097d'
+   or project='0x4d05e3d48a938db4b7a9a59a802d5b45011bde58'
+   or project='0xbafa44efe7901e04e39dad13167d089c559c1138') and token='0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
+
 
 drop table if exists dex_tx_volume_count_summary;
 CREATE TABLE public.dex_tx_volume_count_summary (
