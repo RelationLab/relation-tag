5dce0144b16fa9fb1e1fee6876e976629ed6666f
HP <panzuchao@126.com>
2023-04-20 09:12:32 +0800

    持有最早的时间

--- app/tag-app/src/main/resources/tagscripts/address_label_token_time_first_lp.sql
+++ app/tag-app/src/main/resources/tagscripts/address_label_token_time_first_lp.sql
@@ -42,21 +42,20 @@ from
             row_number() over( partition by a2.token,
 		seq_flag
 	order by
-		first_updated_block_height asc,
+		first_tx_time asc,
 		address asc) as rn
         from
             (
                 select
                     *
                 from
-                    dex_tx_volume_count_summary
+                    snapshot_token_holding_time
                 where
                         type = 'lp'
-                  and balance_usd>0 and address not in (select address from exclude_address)) a1
+                  and  address not in (select address from exclude_address)) a1
                 inner join dim_project_token_type a2
                            on
                                        a1.token = a2.token
-                                   and a1.project = a2.project
                                    and a2.data_subject = 'FIRST_MOVER_LP'
     ) s1
 where
