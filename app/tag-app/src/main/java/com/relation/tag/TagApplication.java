9ff988cd32290cb6f500086f1c0b9d601ff487b5
HP <panzuchao@126.com>
2023-04-21 21:22:25 +0800

    ugc分环境

--- app/tag-app/src/main/java/com/relation/tag/TagApplication.java
+++ app/tag-app/src/main/java/com/relation/tag/TagApplication.java
@@ -16,26 +16,26 @@ public class TagApplication {
 
     public static void main(String[] args) throws Exception {
         ConfigurableApplicationContext ctx = SpringApplication.run(TagApplication.class, args);
-//        TagAddressManager tagAddressManager = ctx.getBean(TagAddressManager.class);
-//        String batchDate = DateUtils.formatDate(new Date(), "YYYY-MM-dd");
-//        if (tagAddressManager.checkResult("tag_result", batchDate)){
-//            log.info("checkResult tag end...........");
-//            System.exit(0);
-//        }
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                try {
-//                    tagAddressManager.refreshAllLabel(batchDate);
-//                } catch (Exception e) {
-//                    throw new RuntimeException(e);
-//                }
-//            }
-//        }).start();
-//        Thread.sleep(120*60*1000);
-//        log.info("check address_labels_json_gin start...........");
-//        tagAddressManager.check("tag_result", 1 * 60 * 1000, batchDate, 1);
-//        log.info("tag end...........");
-//        System.exit(0);
+        TagAddressManager tagAddressManager = ctx.getBean(TagAddressManager.class);
+        String batchDate = DateUtils.formatDate(new Date(), "YYYY-MM-dd");
+        if (tagAddressManager.checkResult("tag_result", batchDate)){
+            log.info("checkResult tag end...........");
+            System.exit(0);
+        }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    tagAddressManager.refreshAllLabel(batchDate);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+        Thread.sleep(120*60*1000);
+        log.info("check address_labels_json_gin start...........");
+        tagAddressManager.check("tag_result", 1 * 60 * 1000, batchDate, 1);
+        log.info("tag end...........");
+        System.exit(0);
     }
 }
