5eaba64d4f03a1ad0a1d7e38a1369ef9296ae52e
HP <panzuchao@126.com>
2023-05-07 18:55:02 +0800

    一百美金以下资产过滤掉

--- app/tag-app/src/main/resources/initsql/token_platform.sql
+++ app/tag-app/src/main/resources/initsql/token_platform.sql
@@ -1,13 +1,11 @@
 DROP TABLE IF EXISTS public.token_platform;
 CREATE TABLE public.token_platform (
-                                       id int8 NOT NULL GENERATED ALWAYS AS IDENTITY,
                                        address varchar NOT NULL,
                                        platform varchar NOT NULL,
                                        platform_name varchar NULL,
                                        created_at timestamptz NULL DEFAULT CURRENT_TIMESTAMP,
                                        updated_at timestamptz NULL DEFAULT CURRENT_TIMESTAMP,
                                        "removed" bool DEFAULT false,
-                                       CONSTRAINT token_platform_pk PRIMARY KEY (id),
                                        CONSTRAINT token_platform_un UNIQUE (address, platform)
 );
 truncate table token_platform;
