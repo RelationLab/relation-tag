363a687fb850a47133d8002220c18ee5ffac382a
HP <panzuchao@126.com>
2023-05-11 15:16:52 +0800

    fix project asset tradeType为ALL的标签情况

--- app/tag-app/src/main/resources/initsql/eth_holding_vol_count.sql
+++ app/tag-app/src/main/resources/initsql/eth_holding_vol_count.sql
@@ -1,48 +1,48 @@
--- drop table if exists eth_holding_vol_count_tmp;
+drop table if exists eth_holding_vol_count;
 -- ALTER TABLE public.eth_holding_vol_count RENAME TO eth_holding_vol_count_tmp;
--- CREATE TABLE public.eth_holding_vol_count (
---                                               address varchar(256) NULL,
---                                               balance numeric(125, 30) NULL,
---                                               total_transfer_count int8 NULL,
---                                               block_height int8 NULL,
---                                               total_transfer_volume numeric(120, 30) NULL,
---                                               status varchar(128) NULL,
---                                               created_at timestamp NULL,
---                                               updated_at timestamp NULL,
---                                               removed bool NULL,
---                                               fail_count int4 NULL,
---                                               error_code int4 NULL,
---                                               error_message text NULL,
---                                               node_name varchar(512) NULL,
---                                               total_transfer_to_count int8 NULL,
---                                               total_transfer_all_count int8 NULL,
---                                               total_transfer_to_volume numeric(120, 30) NULL,
---                                               total_transfer_all_volume numeric(120, 30) NULL
--- ) distributed by (address);
--- truncate table eth_holding_vol_count;
--- vacuum eth_holding_vol_count;
---
--- insert
--- into
---     eth_holding_vol_count(address,
---                           block_height,
---                           total_transfer_volume,
---                           total_transfer_count,
---                           total_transfer_to_count,
---                           total_transfer_all_count,
---                           total_transfer_to_volume,
---                           total_transfer_all_volume)
--- select
---     address,
---     max(block_height) as block_height,
---     sum(total_transfer_volume) total_transfer_volume,
---     sum(total_transfer_count) total_transfer_count,
---     sum(total_transfer_to_count) as total_transfer_to_count,
---     sum(total_transfer_all_count) as total_transfer_all_count,
---     sum(total_transfer_to_volume) as total_transfer_to_volume,
---     sum(total_transfer_all_volume) total_transfer_all_volume
--- from
---     (
+CREATE TABLE public.eth_holding_vol_count (
+                                              address varchar(256) NULL,
+                                              balance numeric(125, 30) NULL,
+                                              total_transfer_count int8 NULL,
+                                              block_height int8 NULL,
+                                              total_transfer_volume numeric(120, 30) NULL,
+                                              status varchar(128) NULL,
+                                              created_at timestamp NULL,
+                                              updated_at timestamp NULL,
+                                              removed bool NULL,
+                                              fail_count int4 NULL,
+                                              error_code int4 NULL,
+                                              error_message text NULL,
+                                              node_name varchar(512) NULL,
+                                              total_transfer_to_count int8 NULL,
+                                              total_transfer_all_count int8 NULL,
+                                              total_transfer_to_volume numeric(120, 30) NULL,
+                                              total_transfer_all_volume numeric(120, 30) NULL
+) distributed by (address);
+truncate table eth_holding_vol_count;
+vacuum eth_holding_vol_count;
+
+insert
+into
+    eth_holding_vol_count(address,
+                          block_height,
+                          total_transfer_volume,
+                          total_transfer_count,
+                          total_transfer_to_count,
+                          total_transfer_all_count,
+                          total_transfer_to_volume,
+                          total_transfer_all_volume)
+select
+    address,
+    max(block_height) as block_height,
+    sum(total_transfer_volume) total_transfer_volume,
+    sum(total_transfer_count) total_transfer_count,
+    sum(total_transfer_to_count) as total_transfer_to_count,
+    sum(total_transfer_all_count) as total_transfer_all_count,
+    sum(total_transfer_to_volume) as total_transfer_to_volume,
+    sum(total_transfer_all_volume) total_transfer_all_volume
+from
+    (
 --         select
 --             address,
 --             max(block_height) as block_height,
@@ -74,24 +74,24 @@
 --         group by
 --             from_address
 --         union all
---         select
---             to_address address,
---             max(block_number) as block_height,
---             0 as total_transfer_volume,
---             0 as total_transfer_count,
---             sum(1) as total_transfer_to_count,
---             sum(1) total_transfer_all_count,
---             sum(amount) as total_transfer_to_volume,
---             sum(amount) total_transfer_all_volume
---         from
---             eth_tx_record etr
---         where
---                 amount>0
---           and etr.block_number > (select block_height from snapshot_dms_syn_block where syn_type='eth_tx_record')
---         group by
---             to_address) atb where address !=''
--- group by
---     address;
+        select
+            to_address address,
+            max(block_number) as block_height,
+            0 as total_transfer_volume,
+            0 as total_transfer_count,
+            sum(1) as total_transfer_to_count,
+            sum(1) total_transfer_all_count,
+            sum(amount) as total_transfer_to_volume,
+            sum(amount) total_transfer_all_volume
+        from
+            eth_tx_record etr
+        where
+                amount>0
+          and etr.block_number > (select block_height from snapshot_dms_syn_block where syn_type='eth_tx_record')
+        group by
+            to_address) atb where address !=''
+group by
+    address;
 insert into tag_result(table_name,batch_date)  SELECT 'eth_holding_vol_count_tmp' as table_name,to_char(current_date ,'YYYY-MM-DD')  as batch_date;
 
 
