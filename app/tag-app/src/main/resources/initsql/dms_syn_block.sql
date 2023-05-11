461f28af62dc900e1801e92bc257a864f8a3e402
HP <panzuchao@126.com>
2023-05-11 23:33:09 +0800

    and address not in (select address from exclude_address)

--- app/tag-app/src/main/resources/initsql/dms_syn_block.sql
+++ app/tag-app/src/main/resources/initsql/dms_syn_block.sql
@@ -7,23 +7,23 @@ CREATE TABLE public.dms_syn_block (
 truncate table dms_syn_block;
 vacuum dms_syn_block;
 insert into dms_syn_block(syn_type,block_height)
-select 'dex_tx_volume_count_record' as syn_type,max(block_height) from dex_tx_volume_count_record;
+select 'dex_tx_volume_count_record' as syn_type,max(block_height) from dex_tx_volume_count_record_cdc;
 insert into dms_syn_block(syn_type,block_height)
-select 'erc20_tx_record' as syn_type,select max(block_number) from erc20_tx_record;
+select 'erc20_tx_record' as syn_type,select max(block_number) from erc20_tx_record_cdc;
 insert into dms_syn_block(syn_type,block_height)
-select 'eth_holding' as syn_type,select max(block_height) from eth_holding;
+select 'eth_holding' as syn_type,select max(block_height) from eth_holding_cdc;
 insert into dms_syn_block(syn_type,block_height)
-select 'eth_tx_record' as syn_type,select max(block_number) from eth_tx_record;
+select 'eth_tx_record' as syn_type,select max(block_number) from eth_tx_record_cdc;
 insert into dms_syn_block(syn_type,block_height)
-select 'nft_buy_sell_holding' as syn_type,select max(updated_block_height)  from nft_buy_sell_holding;
+select 'nft_buy_sell_holding' as syn_type,select max(updated_block_height)  from nft_buy_sell_holding_cdc;
 insert into dms_syn_block(syn_type,block_height)
-select 'nft_holding' as syn_type,select max(updated_block_height)  from nft_holding;
+select 'nft_holding' as syn_type,select max(updated_block_height)  from nft_holding_cdc;
 insert into dms_syn_block(syn_type,block_height)
-select 'token_holding' as syn_type,select max(block_height)   from token_holding;
+select 'token_holding' as syn_type,select max(block_height)   from token_holding_cdc;
 insert into dms_syn_block(syn_type,block_height)
-select 'token_holding_uni' as syn_type,select max(block_height)   from token_holding_uni;
+select 'token_holding_uni' as syn_type,select max(block_height)   from token_holding_uni_cdc;
 insert into dms_syn_block(syn_type,block_height)
-select 'web3_transaction_record' as syn_type,select max(block_height)   from web3_transaction_record;
+select 'web3_transaction_record' as syn_type,select max(block_height)   from web3_transaction_record_cdc;
 
 insert into tag_result(table_name,batch_date)  SELECT 'dms_syn_block' as table_name,to_char(current_date ,'YYYY-MM-DD')  as batch_date;
 
