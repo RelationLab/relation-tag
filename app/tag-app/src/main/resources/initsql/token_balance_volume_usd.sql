8cd45bd569de646f46fd81f40babbd356c5c3003
HP <panzuchao@126.com>
2023-05-23 22:08:24 +0800

    汇总总金额SQL更改

--- app/tag-app/src/main/resources/initsql/token_balance_volume_usd.sql
+++ app/tag-app/src/main/resources/initsql/token_balance_volume_usd.sql
@@ -14,12 +14,14 @@ truncate table token_balance_volume_usd;
 vacuum token_balance_volume_usd;
 
 insert into token_balance_volume_usd(address, token, balance_usd, volume_usd)
-select distinct eh.address as address, 'eth' as token, eh.balance * price as balance_usd , eh.total_transfer_all_volume* price as volume_usd from eth_holding eh
-                                                                                                                                                     inner join white_list_erc20 wle on symbol='WETH' where eh.balance > 0 or eh.total_transfer_all_volume>0;
+select  address,token,balance_usd,volume_usd from (select distinct eh.address as address, 'eth' as token, eh.balance * price as balance_usd ,
+                eh.total_transfer_all_volume* price as volume_usd from eth_holding eh
+         inner join white_list_erc20 wle on symbol='WETH' ) budOut where budOut.balance_usd>=100;
+
 insert into token_balance_volume_usd(address, token, balance_usd, volume_usd)
-select distinct th.address, token, th.balance * price as balance_usd, total_transfer_all_volume* price as volume_usd from token_holding th
-                                                                                                                              inner join white_list_erc20 wle  on th.token = wle.address and ignored = false where
-    (th.balance > 0 or th.total_transfer_all_volume>0) and th.token in (select token_id from dim_rank_token);
+select address,token,balance_usd,volume_usd from  (select distinct th.address, token, th.balance * price as balance_usd, total_transfer_all_volume* price as volume_usd
+   from token_holding th  inner join white_list_erc20 wle  on th.token = wle.address and ignored = false where  th.token in (select token_id from dim_rank_token)) budout
+ where budOut.balance_usd>=100;
 
 insert into tag_result(table_name,batch_date)  SELECT 'token_balance_volume_usd' as table_name,to_char(current_date ,'YYYY-MM-DD')  as batch_date;
 
