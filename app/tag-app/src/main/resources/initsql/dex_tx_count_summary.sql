98e4587797b0e53cf5a5b1f5fead96e3135f17bf
HP <panzuchao@126.com>
2023-06-09 02:24:06 +0800

    vol的计算

--- app/tag-app/src/main/resources/initsql/dex_tx_count_summary.sql
+++ app/tag-app/src/main/resources/initsql/dex_tx_count_summary.sql
@@ -3,6 +3,7 @@ CREATE TABLE public.dex_tx_count_summary (
                                              address varchar(256) NOT NULL,
                                              "token" varchar(256) NOT NULL,
                                              total_transfer_count int8 DEFAULT 0,
+                                             transaction_hash varchar(100) NULL,
                                              created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
                                              updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
                                              removed bool NULL DEFAULT FALSE,
@@ -14,15 +15,15 @@ vacuum dex_tx_count_summary;
 
 INSERT
 INTO
-    dex_tx_volume_count_summary(address,
-                                token,
-                                TYPE,
-                                project,
-                                transaction_hash,
-                                total_transfer_count)
+    dex_tx_count_summary(address,
+                         token,
+                         TYPE,
+                         project,
+                         transaction_hash,
+                         total_transfer_count)
 SELECT
     address,
-    token,
+    'ALL' AS token,
     TYPE,
     project,
     transaction_hash,
@@ -37,8 +38,41 @@ WHERE
             dim_rank_token)
 GROUP BY
     address,
-    token,
     TYPE,
     project,
     transaction_hash;
 
+INSERT
+INTO
+    dex_tx_count_summary(address,
+                         token,
+                         TYPE,
+                         project,
+                         transaction_hash,
+                         total_transfer_count)
+select
+    th.address,
+    'ALL' as token,
+    th.type as type,
+    '0xc36442b4a4522e871399cd717abdd847ab11fe88' as project,
+    transaction_hash,
+    max(total_transfer_count) as total_transfer_count
+from
+    token_holding_uni_cal th
+        inner join (
+        select
+            *
+        from
+            white_list_erc20
+        where
+                address in (
+                select
+                    token_id
+                from
+                    dim_rank_token)) w on
+                w.address = th.price_token
+            and triggered_flag = '1'
+group by
+    th.address,
+    th.type,
+    transaction_hash;
