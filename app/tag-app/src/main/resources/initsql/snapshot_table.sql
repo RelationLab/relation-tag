3e47d93fcc79c96ae581a803f2e28812236aedd2
aisi <aisi>
2023-06-19 13:40:53 +0800

    white_list_lp

--- app/tag-app/src/main/resources/initsql/snapshot_table.sql
+++ app/tag-app/src/main/resources/initsql/snapshot_table.sql
@@ -40,8 +40,40 @@ insert into platform_nft_holding(id, address, platform, quote_token, "token", to
 create table nft_holding_time as select * from nft_holding_time_cdc;
 create table eth_holding_time as select * from eth_holding_time_cdc;
 create table token_holding_time as select * from token_holding_time_cdc;
-create table  white_list_lp as select * from white_list_lp_cdc;
+-- create table  white_list_lp as select * from white_list_lp_cdc;
+CREATE TABLE "public"."white_list_lp" (
+  "id" int8 NOT NULL,
+  "chain_id" int4,
+  "name" varchar(100) COLLATE "pg_catalog"."default",
+  "symbol" varchar(100) COLLATE "pg_catalog"."default",
+  "address" varchar(42) COLLATE "pg_catalog"."default" NOT NULL,
+  "factory" varchar(42) COLLATE "pg_catalog"."default",
+  "pool_id" varchar(100) COLLATE "pg_catalog"."default",
+  "type" varchar(20) COLLATE "pg_catalog"."default",
+  "token0" varchar(42) COLLATE "pg_catalog"."default",
+  "token1" varchar(42) COLLATE "pg_catalog"."default",
+  "tokens" varchar[] COLLATE "pg_catalog"."default",
+  "decimals" int2,
+  "price" numeric(125,18),
+  "tvl" numeric(125,18),
+  "total_supply" numeric(125,18),
+  "remark" varchar(2000) COLLATE "pg_catalog"."default",
+  "created_at" timestamptz(6),
+  "updated_at" timestamptz(6),
+  "removed" bool,
+  "symbol_wired" varchar(100) COLLATE "pg_catalog"."default",
+  "fee" numeric(125,8),
+  "factory_type" varchar(100) COLLATE "pg_catalog"."default",
+  "factory_content" varchar(255) COLLATE "pg_catalog"."default",
+  "symbols" varchar[] COLLATE "pg_catalog"."default"
+);
 create table  white_list_price as select * from white_list_price_cdc;
+
+insert into white_list_lp(
+"id", "chain_id", "name", "symbol", "address", "factory", "pool_id", "type", "token0", "token1", "tokens", "decimals", "price", "tvl", "total_supply", "remark", "created_at", "updated_at", "removed", "symbol_wired", "fee", "factory_type", "factory_content", "symbols"
+) select
+"id", "chain_id", "name", "symbol", "address", "factory", "pool_id", "type", "token0", "token1", "tokens", "decimals", "price", "tvl", "total_supply", "remark", "created_at", "updated_at", "removed", "symbol_wired", "fee", "factory_type", "factory_content", "symbols"
+from white_list_lp_cdc;
 
 update platform_nft_holding set quote_token='0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',weth_token_flag='1' where quote_token='0x0000000000a39bb272e79075ade125fd351887ac';
 
