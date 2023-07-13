39ab50d054633d03f76ece66843d33bdd5797693
HP <panzuchao@126.com>
2023-07-14 01:17:01 +0800

    nft_holding统计

--- app/tag-app/src/main/resources/initsql/platform_nft_holding.sql
+++ app/tag-app/src/main/resources/initsql/platform_nft_holding.sql
@@ -41,5 +41,7 @@ insert into platform_nft_holding (address, platform, quote_token, token, total_t
 update platform_nft_holding set platform_group='LooksRare' where platform = '0x59728544b08ab483533076417fbbb2fd0b17ce3a';
 update platform_nft_holding set platform_group='opensea' where platform in ('0x7be8076f4ea4a4ad08075c2508e481d6c946d12b', '0x00000000006c3852cbef3e08e8df289169ede581', '0x7f268357a8c2552623316e2562d90e642bb538e5');
 update platform_nft_holding set platform_group='X2Y2' where platform = '0x74312363e45dcaba76c59ec49a7aa8a65a67eed3';
+update platform_nft_holding set platform_group='CryptoPunks' where platform = '0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb';
+update platform_nft_holding set platform_group='Blur.io: Marketplace' where platform in ('0x000000000000ad05ccc4f10045630fb830b95127', '0x39da41747a83aee658334415666f3ef92dd0d541');
 
 insert into tag_result(table_name,batch_date)  SELECT 'platform_nft_holding' as table_name,to_char(current_date ,'YYYY-MM-DD')  as batch_date;
