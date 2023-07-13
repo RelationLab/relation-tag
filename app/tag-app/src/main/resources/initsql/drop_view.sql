675dd0d987f57c3a8e03a3cb3f86d527105bc5ae
HP <panzuchao@126.com>
2023-07-13 23:02:12 +0800

    nft_holding统计

--- app/tag-app/src/main/resources/initsql/drop_view.sql
+++ app/tag-app/src/main/resources/initsql/drop_view.sql
@@ -3,6 +3,11 @@ DROP VIEW if EXISTS public.erc20_tx_record;
 DROP VIEW if EXISTS public.eth_tx_record;
 DROP VIEW if EXISTS public.token_holding_uni;
 DROP VIEW if EXISTS public.web3_transaction_record;
+
+DROP VIEW if EXISTS public.nft_holding_record;
+DROP VIEW if EXISTS public.erc721_tx_record;
+DROP VIEW if EXISTS public.platform_nft_tx_record;
+
 insert into tag_result(table_name,batch_date)  SELECT 'drop_view' as table_name,to_char(current_date ,'YYYY-MM-DD')  as batch_date;
 insert into tag_result(table_name,batch_date)  SELECT 'tagging' as table_name,to_char(current_date ,'YYYY-MM-DD')  as batch_date;
 
