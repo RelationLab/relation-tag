84351de0b4abefc28da958744e06b518bc56e743
HP <panzuchao@126.com>
2023-07-14 11:11:48 +0800

    nft_holding统计

--- app/tag-app/src/main/resources/dim/trade_type.sql
+++ app/tag-app/src/main/resources/dim/trade_type.sql
@@ -10,4 +10,6 @@ insert into trade_type(trade_type,trade_type_name,trade_type_alis) values('ALL',
 insert into trade_type(trade_type,trade_type_name,trade_type_alis) values('lp','LP','LP');
 insert into trade_type(trade_type,trade_type_name,trade_type_alis) values('swap','Swap','Swap');
 insert into trade_type(trade_type,trade_type_name,trade_type_alis) values('stake','Stake','Stake');
+insert into trade_type(trade_type,trade_type_name,trade_type_alis) values('stakelp','StakeLP','StakeLP');
+insert into trade_type(trade_type,trade_type_name,trade_type_alis) values('withdraw','withdraw','withdraw');
 insert into tag_result(table_name,batch_date)  SELECT 'trade_type' as table_name,to_char(current_date ,'YYYY-MM-DD')  as batch_date;
