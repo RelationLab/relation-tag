13608a1db55ae2d98436ff9a00670a3ea2da5db1
HP <panzuchao@126.com>
2023-07-16 00:08:42 +0800

    nft_holding统计

--- app/tag-app/src/main/resources/initsql/erc20_tx_record_hash.sql
+++ app/tag-app/src/main/resources/initsql/erc20_tx_record_hash.sql
@@ -26,46 +26,41 @@ insert into
                               total_transfer_all_volume,
                               recent_time_code)
 
+
+insert into
+    erc20_tx_record_hash_test(address,
+                                 token,
+                         block_height,
+                                 total_transfer_volume,
+                                 total_transfer_count,
+                                 total_transfer_to_count,
+                                 total_transfer_all_count,
+                                 total_transfer_to_volume,
+                                 total_transfer_all_volume,
+                                 recent_time_code)
+
 select
     from_address address,
     token,
     max(block_number) as block_height,
     sum(amount) total_transfer_volume,
-    sum(total_transfer_count) total_transfer_count,
+    sum(case
+            when sender = from_address then 1
+            else 0
+        end ) total_transfer_count,
     0 as total_transfer_to_count,
-    sum(total_transfer_all_count) as total_transfer_all_count,
+    sum(case
+            when sender = from_address then 1
+            else 0
+        end ) as total_transfer_all_count,
     0 as total_transfer_to_volume,
     sum(amount) total_transfer_all_volume,
     recent_time_code
-from
-    (
-        select
-            sender,
-            from_address,
-            token,
-            block_number,
-            case
-                when erc20_tx_record.block_number >= recent_time.block_height then amount
-                else 0
-                end as amount,
-            case
-                when sender = from_address
-                    and erc20_tx_record.block_number >= recent_time.block_height then 1
-                else 0
-                end total_transfer_count,
-            case
-                when sender = from_address
-                    and erc20_tx_record.block_number >= recent_time.block_height then 1
-                else 0
-                end total_transfer_all_count,
-            recent_time.recent_time_code
-        from
-            erc20_tx_record
-                inner join recent_time on
-                (1 = 1) ) e20tr
+from erc20_tx_record erc20_tx_record
+         left join (select * from  recent_time where recent_time.recent_time_code='3m' ) recent_time on
+    (erc20_tx_record.block_number >= recent_time.block_height)
 group by
     from_address,
     sender,
     token,
     recent_time_code;
-insert into tag_result(table_name,batch_date)  SELECT 'erc20_tx_record_hash' as table_name,to_char(current_date ,'YYYY-MM-DD')  as batch_date;
