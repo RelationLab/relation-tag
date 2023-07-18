fb0eed7f4f343eb51d029cdeffd34c6cd21c5628
HP <panzuchao@126.com>
2023-07-18 18:32:10 +0800

    nft_holding统计

--- app/tag-app/src/main/resources/initsql/nft_platform.sql
+++ app/tag-app/src/main/resources/initsql/nft_platform.sql
@@ -11,6 +11,15 @@ truncate table nft_platform;
 vacuum nft_platform;
 
 insert into nft_platform (address, platform,platform_name)
-select token, platform,platform_group  from platform_nft_holding
-group by token, platform,platform_group;
+select
+    platform_nft_tx_record.token,
+    mp_nft_platform.platform,
+    mp_nft_platform.platform_name_alis
+from
+    platform_nft_tx_record
+        inner join mp_nft_platform on
+        (platform_nft_tx_record.platform_address = mp_nft_platform.platform)
+group by  platform_nft_tx_record.token,
+          mp_nft_platform.platform,
+          mp_nft_platform.platform_name_alis;
 insert into tag_result(table_name,batch_date)  SELECT 'nft_platform' as table_name,to_char(current_date ,'YYYY-MM-DD')  as batch_date;
