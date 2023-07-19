ecbb3b05b5e07a59f8cd75a032873762e51b6ac6
HP <panzuchao@126.com>
2023-07-19 08:19:10 +0800

    nft_holding统计

--- app/tag-app/src/main/resources/initsql/nft_transfer_holding.sql
+++ app/tag-app/src/main/resources/initsql/nft_transfer_holding.sql
@@ -8,16 +8,17 @@ create table nft_transfer_holding
     total_transfer_count  bigint,
     created_at            timestamp default CURRENT_TIMESTAMP,
     updated_at            timestamp default CURRENT_TIMESTAMP,
-    removed               boolean   default false
+    recent_time_code           varchar(30) NULL
 );
 truncate table nft_transfer_holding;
 vacuum nft_transfer_holding;
 
-insert into nft_transfer_holding (address, token, total_transfer_volume, total_transfer_count)
+insert into nft_transfer_holding (address, token, total_transfer_volume, total_transfer_count,recent_time_code)
     (select nh.address,
             nh.token,
             nh.total_transfer_all_volume - nh.total_transfer_mint_volume - nh.total_transfer_burn_volume - COALESCE(nbsh.total_transfer_buy_volume,0) - COALESCE(nbsh.total_transfer_sell_volume,0),
-            nh.total_transfer_all_count - nh.total_transfer_mint_count - nh.total_transfer_burn_count - COALESCE(nbsh.total_transfer_buy_count,0) - COALESCE(nbsh.total_transfer_sell_count,0)
+            nh.total_transfer_all_count - nh.total_transfer_mint_count - nh.total_transfer_burn_count - COALESCE(nbsh.total_transfer_buy_count,0) - COALESCE(nbsh.total_transfer_sell_count,0),
+            nh.recent_time_code
      from nft_holding nh
               left join nft_buy_sell_holding nbsh
                         on nh.address = nbsh.address
