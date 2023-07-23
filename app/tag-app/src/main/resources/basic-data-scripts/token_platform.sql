518bb49a433bf30661f68856fa0d8020e58c118f
HP <panzuchao@126.com>
2023-07-23 09:02:14 +0800

    platform_lend_tx_record

--- app/tag-app/src/main/resources/basic-data-scripts/token_platform.sql
+++ app/tag-app/src/main/resources/basic-data-scripts/token_platform.sql
@@ -7,6 +7,17 @@ where (project='0xae7ab96520de3a18e5e111b5eaab095312d7fe84'
     or project='0xbafa44efe7901e04e39dad13167d089c559c1138')
   and token='0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
 
+update dex_tx_volume_count_record_cdc set token='eth'
+where (project='0xae7ab96520de3a18e5e111b5eaab095312d7fe84'
+    or project='0x4d05e3d48a938db4b7a9a59a802d5b45011bde58'
+    or project='0xbafa44efe7901e04e39dad13167d089c559c1138')
+  and (token='0xae7ab96520de3a18e5e111b5eaab095312d7fe84'
+    or token='0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0'
+    or token='0xae78736cd615f374d3085123a210448e74fc6393'
+    or token='0xac3e018457b222d93114458476f3e3416abbe38f'
+    or token='0x5e8422345238f34275888049021821e8e08caa1f')
+  and type='withdraw';
+
 DROP TABLE IF EXISTS public.token_platform;
 CREATE TABLE public.token_platform (
                                        address varchar NOT NULL,
