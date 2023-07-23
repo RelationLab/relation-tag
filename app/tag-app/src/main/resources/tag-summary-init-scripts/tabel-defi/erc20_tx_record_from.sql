14b541cf9920b609e5dab01b6521e553193d8010
HP <panzuchao@126.com>
2023-07-23 12:25:23 +0800

    platform_lend_tx_record

--- app/tag-app/src/main/resources/tag-summary-init-scripts/tabel-defi/erc20_tx_record_from.sql
+++ app/tag-app/src/main/resources/tag-summary-init-scripts/tabel-defi/erc20_tx_record_from.sql
@@ -7,7 +7,7 @@ CREATE TABLE public.erc20_tx_record_from(
                                                  total_transfer_count bigint NULL,
                                                  created_at timestamp NULL,
                                                  recent_time_code varchar(30) NULL
-) distributed by (address,"token");
+) distributed by (address,"token",recent_time_code);
 truncate table erc20_tx_record_from;
 vacuum erc20_tx_record_from;
 insert into tag_result(table_name,batch_date)  SELECT 'erc20_tx_record_from' as table_name,to_char(current_date ,'YYYY-MM-DD')  as batch_date;
--- app/tag-app/src/main/resources/tag-summary-init-scripts/tabel-defi/erc20_tx_record_to.sql
+++ app/tag-app/src/main/resources/tag-summary-init-scripts/tabel-defi/erc20_tx_record_to.sql
@@ -7,7 +7,7 @@ CREATE TABLE public.erc20_tx_record_to(
                                             total_transfer_count bigint NULL,
                                             created_at timestamp NULL,
                                             recent_time_code varchar(30) NULL
-) distributed by (address,"token");
+) distributed by (address,"token",recent_time_code);
 truncate table erc20_tx_record_to;
 vacuum erc20_tx_record_to;
 insert into tag_result(table_name,batch_date)  SELECT 'erc20_tx_record_to' as table_name,to_char(current_date ,'YYYY-MM-DD')  as batch_date;
