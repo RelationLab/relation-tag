03772d0b49c7c36ed1ba1eba23b548d2c3d20058
HP <panzuchao@126.com>
2023-07-26 01:15:56 +0800

    platform_lend_tx_record

--- app/tag-app/src/main/resources/tag-summary-init-scripts/recent-time/erc20_tx_record_from.sql
+++ app/tag-app/src/main/resources/tag-summary-init-scripts/recent-time/erc20_tx_record_from.sql
@@ -9,12 +9,15 @@ select
     from_address address,
     token,
     max(block_number) as block_height,
-    sum(total_transfer_volume) total_transfer_volume,
+    sum(amount) total_transfer_volume,
     1 total_transfer_count,
     recent_time_code
-from erc20_tx_record_filter
+from erc20_tx_record
+         inner join (select * from top_token_1000 where holders >= 100 and removed <> 'true') top_token_1000
+                    on(erc20_tx_record.token = top_token_1000.address)
          inner join (select * from  recent_time where recent_time.recent_time_code='${recent_time_code}' ) recent_time on
-    (erc20_tx_record_filter.block_number >= recent_time.block_height)
+        (erc20_tx_record.block_number >= recent_time.block_height)
+        and from_address=sender
 group by
     from_address,
     sender,
--- app/tag-app/src/main/resources/tag-summary-init-scripts/recent-time/erc20_tx_record_to.sql
+++ app/tag-app/src/main/resources/tag-summary-init-scripts/recent-time/erc20_tx_record_to.sql
@@ -10,19 +10,22 @@ select
     to_address address,
     token,
     max(block_number) as block_height,
-    sum(total_transfer_volume) as total_transfer_volume,
+    sum(amount) as total_transfer_volume,
     0 as total_transfer_count,
     recent_time_code
 from
-    erc20_tx_record_filter
+    erc20_tx_record
+        inner join (select * from top_token_1000 where holders >= 100 and removed <> 'true') top_token_1000
+                   on(erc20_tx_record.token = top_token_1000.address)
         inner join (
         select
             *
         from
             recent_time
         where
-                recent_time.recent_time_code = '${recent_time_code}' ) recent_time on
-        (erc20_tx_record_filter.block_number >= recent_time.block_height)
+                recent_time.recent_time_code = '1m' ) recent_time on
+        (erc20_tx_record.block_number >= recent_time.block_height)
+where to_address=sender
 group by
     to_address,
     token,
