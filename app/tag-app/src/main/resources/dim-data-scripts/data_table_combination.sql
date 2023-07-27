eb57fae49321d1e0bcab7c26e1a722107d6e86a7
HP <panzuchao@126.com>
2023-07-28 00:41:42 +0800

    combination

--- app/tag-app/src/main/resources/dim-data-scripts/data_table_combination.sql
+++ app/tag-app/src/main/resources/dim-data-scripts/data_table_combination.sql
@@ -22,6 +22,5 @@ CREATE TABLE public.combination_temp (
                                     label_category varchar(50) NULL,
                                     recent_time_code varchar(30) NULL
 );
-CREATE INDEX combination_temp_idx_label_name ON public.combination_temp USING btree (label_name);
 update nft_sync_address set platform='CryptoPunks' where  address='0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb';
 insert into tag_result(table_name,batch_date)  SELECT 'data_table_combination' as table_name,'${batchDate}'  as batch_date;
