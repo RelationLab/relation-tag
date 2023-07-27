fe469e2b670791dda8074f338a1425e212edca2a
HP <panzuchao@126.com>
2023-07-27 12:25:16 +0800

    platform_lend_tx_record

--- app/tag-app/src/main/resources/tag-summary-init-scripts/recent-time/dex_tx_count_summary.sql
+++ app/tag-app/src/main/resources/tag-summary-init-scripts/recent-time/dex_tx_count_summary.sql
@@ -69,27 +69,4 @@ group by
     address,
     type;
 
-
-INSERT
-INTO
-    dex_tx_count_summary(address,
-                         token,
-                         TYPE,
-                         project,
-                         total_transfer_count,
-                         recent_time_code)
-SELECT
-    address,
-    'ALL' AS token,
-    'ALL' as TYPE,
-    project,
-    sum(total_transfer_count) AS total_transfer_count,
-    recent_time_code
-FROM
-    dex_tx_count_summary
-GROUP BY
-    address,
-    project,
-    recent_time_code;
-
 insert into tag_result(table_name,batch_date)  SELECT 'dex_tx_count_summary_${recentTimeCode}' as table_name,to_char(current_date ,'YYYY-MM-DD')  as batch_date;
