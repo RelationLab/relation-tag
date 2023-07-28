a7d601da1e6cc5fb030a26feca6455c58bedab22
HP <panzuchao@126.com>
2023-07-28 12:33:43 +0800

    nft_holding_temp

--- app/tag-app/src/main/resources/tag-summary-init-scripts/recent-time/platform_nft_type_volume_count.sql
+++ app/tag-app/src/main/resources/tag-summary-init-scripts/recent-time/platform_nft_type_volume_count.sql
@@ -86,6 +86,44 @@ from (
              (pltr.lend_token = nft_sync_address.address)
          where pltr.block_number >= ${recentTimeBlockHeight}
          group by pltr.lender,
+                  pltr.lend_token
+         union all
+         select address,
+                "token"             as quote_token,
+                "pledge_token",
+                sum(volume_usd)     as volume_usd,
+                sum(transfer_count) as transfer_count
+         from (select pltr.borrower   as address,
+                      pltr.lend_token as "token",
+                      pltr."type"     as "type",
+                      sum(1)          as volume_usd,
+                      1               as transfer_count
+               from platform_lend_tx_record pltr
+                        inner join (select address
+                                    from nft_sync_address nsa
+                                    where type = 'ERC721') nft_sync_address on
+                   (pltr.lend_token = nft_sync_address.address)
+               where pltr.block_number >= ${recentTimeBlockHeight}
+               group by pltr.borrower,
+                        pltr.lend_token,
+                        pltr."type",
+                        hash) pltrout
+         group by pltrout.address,
+                  pltrout.token
+                  ----------------增加blur的lend的to
+         union all
+         select pltr.lender     as address,
+                pltr.lend_token as quote_token,
+                pltr.pledge_token as "token",
+                sum(1)          as volume_usd,
+                0               as transfer_count
+         from platform_lend_tx_record pltr
+                  inner join (select address
+                              from nft_sync_address nsa
+                              where type = 'ERC721') nft_sync_address on
+             (pltr.lend_token = nft_sync_address.address)
+         where pltr.block_number >= ${recentTimeBlockHeight}
+         group by pltr.lender,
                   pltr.lend_token) lendt
 group by lendt.address,
          lendt.token;
