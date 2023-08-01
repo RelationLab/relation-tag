8d7b32e1128414b958bd448d56b6699e4d566c75
HP <panzuchao@126.com>
2023-08-01 17:54:52 +0800

    blur balance

---
+++ app/tag-app/src/main/resources/tag-summary-init-scripts/rename_table_dev.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS address_label_gp_${tableSuffix};
+ALTER TABLE address_label_gp_temp_${tableSuffix}                    RENAME TO address_label_gp_${tableSuffix};
+DROP TABLE IF EXISTS address_labels_json_gin_${tableSuffix};
+ALTER TABLE address_labels_json_gin_temp_${tableSuffix}              RENAME TO address_labels_json_gin_${tableSuffix} ;
+insert into tag_result(table_name,batch_date)  SELECT 'rename_table_${tableSuffix}' as table_name,'${batchDate}'  as batch_date;
+delete from  tag_result where  table_name='tagging';
+delete from tag_result where batch_date<'${batchDate}';
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly;
