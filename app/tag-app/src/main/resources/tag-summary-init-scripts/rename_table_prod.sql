88bead2bbc6dc7b8eaa52bbb018deefc3b65aa56
HP <panzuchao@126.com>
2023-08-03 19:00:54 +0800

    '||nft_action_platform_temp.nft_type||'

--- app/tag-app/src/main/resources/tag-summary-init-scripts/rename_table_prod.sql
+++ app/tag-app/src/main/resources/tag-summary-init-scripts/rename_table_prod.sql
@@ -4,5 +4,6 @@ DROP TABLE IF EXISTS address_labels_json_gin_${tableSuffix};
 ALTER TABLE address_labels_json_gin_temp_${tableSuffix}              RENAME TO address_labels_json_gin_${tableSuffix} ;
 insert into tag_result(table_name,batch_date)  SELECT 'rename_table_${tableSuffix}' as table_name,'${batchDate}'  as batch_date;
 delete from  tag_result where  table_name='tagging';
+delete  from tag_result where table_name='tag-begin'
 delete from tag_result where batch_date<'${batchDate}';
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly;
