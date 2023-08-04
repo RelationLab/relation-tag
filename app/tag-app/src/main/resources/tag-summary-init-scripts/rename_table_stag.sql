fbffbed5404cdae22e96f478c98911e27b65c9d7
HP <panzuchao@126.com>
2023-08-04 08:24:17 +0800

    '||nft_action_platform_temp.nft_type||'

--- app/tag-app/src/main/resources/tag-summary-init-scripts/rename_table_stag.sql
+++ app/tag-app/src/main/resources/tag-summary-init-scripts/rename_table_stag.sql
@@ -108,6 +108,6 @@ ALTER TABLE nft_volume_count_temp                       RENAME TO nft_volume_cou
 
 insert into tag_result(table_name,batch_date)  SELECT 'rename_table_${tableSuffix}' as table_name,'${batchDate}'  as batch_date;
 delete from  tag_result where  table_name='tagging';
-delete  from tag_result where table_name='tag-begin'
+delete  from tag_result where table_name='tag-begin';
 delete from tag_result where batch_date<'${batchDate}';
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly;
