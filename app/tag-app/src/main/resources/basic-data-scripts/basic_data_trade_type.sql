c372fd720d08eb2e90e2a0c90212a464ec08d713
HP <panzuchao@126.com>
2023-08-04 11:15:45 +0800

    '||nft_action_platform_temp.nft_type||'

--- app/tag-app/src/main/resources/basic-data-scripts/basic_data_trade_type.sql
+++ app/tag-app/src/main/resources/basic-data-scripts/basic_data_trade_type.sql
@@ -13,10 +13,10 @@ insert into trade_type(trade_type,trade_type_name,trade_type_alis) values('stake
 insert into trade_type(trade_type,trade_type_name,trade_type_alis) values('stakelp','StakeLP','StakeLP');
 insert into trade_type(trade_type,trade_type_name,trade_type_alis) values('withdraw','Withdraw','Withdraw');
 
-insert into trade_type(trade_type,trade_type_name,trade_type_alis) values('deposit','deposit','deposit');
-insert into trade_type(trade_type,trade_type_name,trade_type_alis) values('redeem','redeem','redeem');
-insert into trade_type(trade_type,trade_type_name,trade_type_alis) values('borrow','borrow','borrow');
-insert into trade_type(trade_type,trade_type_name,trade_type_alis) values('repay','repay','repay');
-insert into trade_type(trade_type,trade_type_name,trade_type_alis) values('mint','mint','mint');
+insert into trade_type(trade_type,trade_type_name,trade_type_alis) values('deposit','Deposit','deposit');
+insert into trade_type(trade_type,trade_type_name,trade_type_alis) values('redeem','Redeem','redeem');
+insert into trade_type(trade_type,trade_type_name,trade_type_alis) values('borrow','Borrow','borrow');
+insert into trade_type(trade_type,trade_type_name,trade_type_alis) values('repay','Repay','repay');
+insert into trade_type(trade_type,trade_type_name,trade_type_alis) values('mint','Mint','mint');
 
 insert into tag_result(table_name,batch_date)  SELECT 'basic_data_trade_type' as table_name,'${batchDate}'  as batch_date;
