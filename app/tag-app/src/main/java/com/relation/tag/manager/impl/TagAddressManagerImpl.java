75e39c1ca82bbc45cd94e26930be4eda1da89e87
HP <panzuchao@126.com>
2023-08-09 22:35:51 +0800

    dex lending eth2.0分开

--- app/tag-app/src/main/java/com/relation/tag/manager/impl/TagAddressManagerImpl.java
+++ app/tag-app/src/main/java/com/relation/tag/manager/impl/TagAddressManagerImpl.java
@@ -257,7 +257,8 @@ public class TagAddressManagerImpl implements TagAddressManager {
     @Override
     public void tagMerge(String batchDate) throws Exception {
         execSql(null, "address_label_gp_"+configEnvironment+".sql", batchDate, TAG_SUMMARY_INIT_SCRIPTS_PATH, Maps.newHashMap("tableSuffix",configEnvironment), false);
-        execSql("address_label_gp_"+configEnvironment, "address_labels_json_gin_"+configEnvironment+".sql", batchDate, TAG_SUMMARY_INIT_SCRIPTS_PATH, Maps.newHashMap("tableSuffix",configEnvironment), false);
+        execSql("address_label_gp_"+configEnvironment, "address_labels_json_gin_row_"+configEnvironment+".sql", batchDate, TAG_SUMMARY_INIT_SCRIPTS_PATH, Maps.newHashMap("tableSuffix",configEnvironment), false);
+        execSql("address_labels_json_gin_row_"+configEnvironment, "address_labels_json_gin_"+configEnvironment+".sql", batchDate, TAG_SUMMARY_INIT_SCRIPTS_PATH, Maps.newHashMap("tableSuffix",configEnvironment), false);
         execSql("address_labels_json_gin_"+configEnvironment, "rename_table_"+configEnvironment+".sql", batchDate, TAG_SUMMARY_INIT_SCRIPTS_PATH, Maps.newHashMap("tableSuffix",configEnvironment), false);
         if (!StringUtils.equals(configEnvironment,"stag")){
             return;
--- app/tag-app/src/main/resources/tag-summary-init-scripts/address_labels_json_gin_dev.sql
+++ app/tag-app/src/main/resources/tag-summary-init-scripts/address_labels_json_gin_dev.sql
@@ -1,39 +1,42 @@
 DROP TABLE IF EXISTS address_labels_json_gin_temp_${tableSuffix} CASCADE;
 CREATE TABLE address_labels_json_gin_temp_${tableSuffix}
 (
-    id      BIGSERIAL,
-    address TEXT  NOT NULL,
-    data    TEXT NOT NULL
-) WITH (appendoptimized = true, orientation = column) DISTRIBUTED BY (address);
+    id       BIGSERIAL,
+    address  TEXT NOT NULL,
+    data     TEXT NOT NULL,
+    data_3d  TEXT NOT NULL,
+    data_7d  TEXT NOT NULL,
+    data_15d TEXT NOT NULL,
+    data_1m  TEXT NOT NULL,
+    data_3m  TEXT NOT NULL,
+    data_6m  TEXT NOT NULL,
+    data_1y  TEXT NOT NULL,
+    data_2y  TEXT NOT NULL
+) WITH (appendoptimized = true, orientation = column) DISTRIBUTED BY
+(
+    address
+);
 CREATE INDEX idx_address_labels_json_gin_temp_${tableSuffix}_id_${batchDateReplace} ON address_labels_json_gin_temp_${tableSuffix}(id);
 truncate table address_labels_json_gin_temp_${tableSuffix};
 vacuum address_labels_json_gin_temp_${tableSuffix};
 
-INSERT INTO address_labels_json_gin_temp_${tableSuffix}(address, data)
-SELECT address_label_gp_temp_${tableSuffix}.address,
-       JSONB_BUILD_OBJECT(
-               'address', address_label_gp_temp_${tableSuffix}.address,
-               'address_type', CASE WHEN COUNT(contract_address) > 0 THEN 'c' ELSE 'p' END,
-               'labels', JSONB_AGG(
-                       JSONB_BUILD_OBJECT(
-                               'type', label_type,
-                               'name', label_name,
-                               'wired_type', wired_type,
-                               'data', data :: TEXT,
-                               'group', "group",
-                               'level', level,
-                               'category', category,
-                               'trade_type', trade_type,
-                               'project', project,
-                               'asset', asset
-                           )
-                           ORDER BY label_type DESC
-                   ),
-               'updated_at', CURRENT_TIMESTAMP
-           )::TEXT
-FROM address_label_gp_temp_${tableSuffix}
-         LEFT JOIN contract ON (address_label_gp_temp_${tableSuffix}.address = contract.contract_address)
-GROUP BY (address_label_gp_temp_${tableSuffix}.address);
+INSERT INTO address_labels_json_gin_temp_${tableSuffix}(address, data, data_3d, data_7d, data_15d, data_1m, data_3m,
+                                                        data_6m, data_1y, data_2y)
+SELECT address,
+       max(CASE WHEN recent_time_code = 'ALL' THEN data ELSE '' END) AS data,
+       max ( CASE WHEN recent_time_code = '3d' THEN data ELSE '' END) AS data_3d,
+       max ( CASE WHEN recent_time_code = '7d' THEN data ELSE '' END) AS data_7d,
+       max ( CASE WHEN recent_time_code = '15d' THEN data ELSE '' END) AS data_15d,
+       max ( CASE WHEN recent_time_code = '1m' THEN data ELSE '' END) AS data_1m,
+       max ( CASE WHEN recent_time_code = '3m' THEN data ELSE '' END) AS data_3m,
+       max ( CASE WHEN recent_time_code = '6m' THEN data ELSE '' END) AS data_6m,
+       max ( CASE WHEN recent_time_code = '1y' THEN data ELSE '' END) AS data_1y,
+       max ( CASE WHEN recent_time_code = '2y' THEN data ELSE '' END) AS data_2y
+FROM
+    address_labels_json_gin_row_temp_${tableSuffix}
+GROUP BY address;
 
-insert into tag_result(table_name,batch_date)  SELECT 'address_labels_json_gin_${tableSuffix}' as table_name,'${batchDate}'  as batch_date;
-insert into tag_result(table_name,batch_date)  SELECT 'tagging' as table_name,'${batchDate}'  as batch_date;
+insert into tag_result(table_name, batch_date)
+SELECT 'address_labels_json_gin_${tableSuffix}' as table_name, '${batchDate}' as batch_date;
+insert into tag_result(table_name, batch_date)
+SELECT 'tagging' as table_name, '${batchDate}' as batch_date;
