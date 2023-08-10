8d460be4f6c9fc7a51f5cb1e2021495e5a2c94c1
HP <panzuchao@126.com>
2023-08-10 09:38:57 +0800

    dex lending eth2.0分开

--- app/tag-app/src/main/resources/tag-summary-init-scripts/recent-time/erc20_tx_record_from.sql
+++ app/tag-app/src/main/resources/tag-summary-init-scripts/recent-time/erc20_tx_record_from.sql
@@ -18,7 +18,7 @@ select
 from erc20_tx_record
 where erc20_tx_record.block_number >= ${recentTimeBlockHeight}
 and erc20_tx_record.hash not in(
-            ---------OMG--------------
+        ---------OMG--------------
         '0x8fd7391b8d8ceeb907f891b0e2fc479206c837b06d3446facf03794161e9d75c',
         '0xad9e98bc7b4146d19473a1ec8601d946840b91d0c2a8da711841cdfa3739b810',
         ---------MANA--- 0x0f5d2fb29fb7d3cfee444a200298f468908cc942-----------
@@ -34,6 +34,9 @@ and erc20_tx_record.hash not in(
         '0x51dce51ee7738db8acb5de7c366ff876aeeb4a170a3f89ddb02a3d3e5d376e12',
         '0xd36940dad40aa23e0a0184390090ce40e5f35c26aafa6c28dc67a8698ea6ef00',
         '0x573e3a0046fd806fd56cebdee77fa1af6d0cacc972debc850ce7e42d40394a75',
+        '0x7f0877f6b2ea17a496bff75e75d0f39a73d385935f89a0c67607853928b25721',
+        '0x83ec20f4fe947efb69982ee5b3a5bb9841d07bd3725ca6eaf45fd2eeb0387cda',
+        '0x561551aaada7cddd98641a29730f810314bd57d863dfae2e97fa634dadb57a4e',
         ---------Dentacoin--- 0x08d32b0da63e2c3bcf8019c9c5d849d7a9d791e6-----------
         '0x226b59db0805cbbf90023ecac07c9b16f387e5e4d86d18e43494679c67d808d0',
         '0x5402f1c7031adc0032691d6199bcc420579886cdb29d01e2bb0435cb8c74c971',
