f7bd25eaacb2c1f41a7f8605ae28236f6e3a09a3
HP <panzuchao@126.com>
2023-08-13 09:44:54 +0800

    dex lending eth2.0分开

--- app/tag-app/src/main/resources/basic-data-scripts/basic_data_recent_time.sql
+++ app/tag-app/src/main/resources/basic-data-scripts/basic_data_recent_time.sql
@@ -1,6 +1,7 @@
 DROP TABLE if EXISTS  recent_time_temp;
 create table recent_time_temp
 (
+    code varchar(10) NULL,
     recent_time_code varchar(80) NULL,
     recent_time_name varchar(80) NULL,
     recent_time_content varchar(80) NULL,
@@ -9,15 +10,15 @@ create table recent_time_temp
     seg_flag varchar(1)  NULL
 ) ;
 
-insert into recent_time_temp(recent_time_code,recent_time_content,recent_time_name,days) values('3d','3-day','3d',3);
-insert into recent_time_temp(recent_time_code,recent_time_content,recent_time_name,days) values('7d','Weekly','7d',7);
-insert into recent_time_temp(recent_time_code,recent_time_content,recent_time_name,days) values('15d','15-day','15d',15);
-insert into recent_time_temp(recent_time_code,recent_time_content,recent_time_name,days) values('1m','1 Month','1m',30);
-insert into recent_time_temp(recent_time_code,recent_time_content,recent_time_name,days) values('3m','3 Months','3m',90);
-insert into recent_time_temp(recent_time_code,recent_time_content,recent_time_name,days) values('6m','6 Months','6m',180);
-insert into recent_time_temp(recent_time_code,recent_time_content,recent_time_name,days) values('1y','1 Year','1y',365);
-insert into recent_time_temp(recent_time_code,recent_time_content,recent_time_name,days,seg_flag) values('2y','2 Year','2y',730,'1');
-insert into recent_time_temp(recent_time_code,recent_time_content,recent_time_name,block_height,seg_flag) values('ALL','','',46147,'1');
+insert into recent_time_temp(code,recent_time_code,recent_time_content,recent_time_name,days) values('3d','3d','3-day','3d',3);
+insert into recent_time_temp(code,recent_time_code,recent_time_content,recent_time_name,days) values('7d','7d','Weekly','7d',7);
+insert into recent_time_temp(code,recent_time_code,recent_time_content,recent_time_name,days) values('15d','15d','15-day','15d',15);
+insert into recent_time_temp(code,recent_time_code,recent_time_content,recent_time_name,days) values('1m','1m','1 Month','1m',30);
+insert into recent_time_temp(code,recent_time_code,recent_time_content,recent_time_name,days) values('3m','3m','3 Months','3m',90);
+insert into recent_time_temp(code,recent_time_code,recent_time_content,recent_time_name,days) values('6m','6m','6 Months','6m',180);
+insert into recent_time_temp(code,recent_time_code,recent_time_content,recent_time_name,days) values('1y','1y','1 Year','1y',365);
+insert into recent_time_temp(code,recent_time_code,recent_time_content,recent_time_name,days,seg_flag) values('2y','2y','2 Year','2y',730,'1');
+insert into recent_time_temp(code,recent_time_code,recent_time_content,recent_time_name,block_height,seg_flag) values('0a','ALL','','',46147,'1');
 
 update
     recent_time_temp rt
