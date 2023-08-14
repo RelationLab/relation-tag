e1a9b924ad7ecb16718bddeb370061d01058d2dd
HP <panzuchao@126.com>
2023-08-14 11:46:53 +0800

    dex lending eth2.0分开

--- app/tag-app/src/main/resources/wide-table/wired_address_dataset.sql
+++ app/tag-app/src/main/resources/wide-table/wired_address_dataset.sql
@@ -1,14 +1,14 @@
 truncate table  wired_address_dataset_temp;
 insert  into  wired_address_dataset_temp
-select 
-     s1.b_type
-	,s1.statistical_type
-    ,s1.address
-    ,s1.project
-    ,s1.platform_name
-	,s1.token
-    ,case when s1.token='ALL' then 'ALL' else  s2.name end  as asset   -- token 名称
-    ,s1.type as action
+select
+    s1.b_type
+     ,s1.statistical_type
+     ,s1.address
+     ,s1.project
+     ,s1.platform_name
+     ,s1.token
+     ,case when s1.token='ALL' then 'ALL' else  s2.name end  as asset   -- token 名称
+     ,s1.type as action
 	,s1.first_tx_time                             --最早交易时间
     ,s1.latest_tx_time                            -- 最后交易时间
 	,s1.transaction_count
@@ -31,58 +31,69 @@ select
 	,s1.transaction_count_6m 
 	,s1.transaction_count_1y 
 	,s1.transaction_count_2y 	
-    ,now() as etl_update_time     
-from dws_eth_index_n  s1 left join  (select address,name from top_token_1000 where removed=false and holders >= 100) s2 
+    ,now() as etl_update_time
+from dws_eth_index_n  s1 left join  (
+    select address,name from top_token_1000 where removed=false and holders >= 100
+    union all
+    select       distinct
+    wlp.address,
+    wlp.symbol_wired  as name
+    from white_list_lp wlp
+    left join white_list_lp wslp on wlp.address = wslp.address and wlp.type = 'LP' and wslp.type = 'SLP'
+    where wlp.tvl > 1000000
+    and string_to_array(wlp.symbol_wired, '/') && array['ETH','WETH', 'UNI', 'AAVE', '1INCH', 'MANA', 'AXS', 'SAND']
+    and wlp."type" = 'LP'
+    ) s2
 on s1.token=s2.address
 ;
 
 
 insert  into  wired_address_dataset_temp
-select 
-     s1.b_type
-	,s1.statistical_type
-    ,s1.address
-    ,s1.project
-    ,s1.platform_name
-	,s1.token
-    , '' as  asset   -- token 名称
-   , s1.type as action
+select
+    s1.b_type
+     ,s1.statistical_type
+     ,s1.address
+     ,s1.project
+     ,s1.platform_name
+     ,s1.token
+     ,'ALL' as  asset   -- token 名称
+     , s1.type as action
 	,s1.first_tx_time                             --最早交易时间
     ,s1.latest_tx_time                            -- 最后交易时间
 	,s1.transaction_count
 	,s1.transaction_volume
 	,s1.balance_count
    	,s1.balance_usd
-	,s1.transaction_volume_3d 
-	,s1.transaction_volume_7d 
+	,s1.transaction_volume_3d
+	,s1.transaction_volume_7d
 	,s1.transaction_volume_15d
-	,s1.transaction_volume_1m 
-	,s1.transaction_volume_3m 
-	,s1.transaction_volume_6m 
-	,s1.transaction_volume_1y 
-	,s1.transaction_volume_2y 
-	,s1.transaction_count_3d 
-	,s1.transaction_count_7d 
+	,s1.transaction_volume_1m
+	,s1.transaction_volume_3m
+	,s1.transaction_volume_6m
+	,s1.transaction_volume_1y
+	,s1.transaction_volume_2y
+	,s1.transaction_count_3d
+	,s1.transaction_count_7d
 	,s1.transaction_count_15d
-	,s1.transaction_count_1m 
-	,s1.transaction_count_3m 
-	,s1.transaction_count_6m 
-	,s1.transaction_count_1y 
-	,s1.transaction_count_2y 	
+	,s1.transaction_count_1m
+	,s1.transaction_count_3m
+	,s1.transaction_count_6m
+	,s1.transaction_count_1y
+	,s1.transaction_count_2y
     ,now() as etl_update_time
-  from dws_web3_index_n  s1 
+from dws_web3_index_n  s1
 ;
-  
+
 insert  into  wired_address_dataset_temp
-select 
-     s1.b_type
-	,s1.statistical_type
-    ,s1.address
-    ,s1.project
-    ,s1.platform_name
-	,s1.token
-    ,s2.platform  as asset   -- token 名称
-    ,s1.type as action
+select
+    s1.b_type
+     ,s1.statistical_type
+     ,s1.address
+     ,s1.project
+     ,s1.platform_name
+     ,s1.token
+     ,case when s1.token='ALL' then 'ALL' else  s2.platform end   as asset   -- token 名称
+     ,s1.type as action
 	,s1.first_tx_time                             --最早交易时间
     ,s1.latest_tx_time                            -- 最后交易时间
 	,s1.transaction_count
@@ -106,8 +117,8 @@ select
 	,s1.transaction_count_1y 
 	,s1.transaction_count_2y 	
     ,now() as etl_update_time
-  from dws_nft_index_n s1  left join 
- nft_sync_address s2
- on s1.token=s2.address
- ;
+from dws_nft_index_n s1  left join
+    nft_sync_address s2
+on s1.token=s2.address
+;
 insert into tag_result(table_name,batch_date)  SELECT 'wired_address_dataset' as table_name,'${batchDate}'  as batch_date;
