9ec998df019a4a539ffcce5c7293de0644f8d72f
HP <panzuchao@126.com>
2023-08-23 20:36:14 +0800

    and recent_time_code ='ALL'

--- app/tag-app/src/main/resources/tagscripts/address_label_nft_balance_usd_grade.sql
+++ app/tag-app/src/main/resources/tagscripts/address_label_nft_balance_usd_grade.sql
@@ -1,108 +1,108 @@
-drop table if exists address_label_nft_balance_usd_grade;
-CREATE TABLE public.address_label_nft_balance_usd_grade
-(
-    address          varchar(512) NULL,
-    data             numeric(250, 20) NULL,
-    wired_type       varchar(20) NULL,
-    label_type       varchar(512) NULL,
-    label_name       varchar(1024) NULL,
-    updated_at       timestamp(6) NULL,
-    "group"          varchar(1) NULL,
-    "level"          varchar(100) NULL,
-    category         varchar(100) NULL,
-    trade_type       varchar(100) NULL,
-    project          varchar(100) NULL,
-    asset            varchar(100) NULL,
-    bus_type         varchar(20) NULL,
-    recent_time_code varchar(30) NULL
-) with (appendonly = 'true', compresstype = zstd, compresslevel = '5')
-    distributed by
-(
-    address,
-    label_name,
-    recent_time_code
-);
-truncate table public.address_label_nft_balance_usd_grade;
-vacuum
-address_label_nft_balance_usd_grade;
-
-insert into public.address_label_nft_balance_usd_grade(address, label_type, label_name, data, wired_type, updated_at,
-                                                       "group", level, category, trade_type, project, asset, bus_type)
-select address
-        ,
-       label_type
-        ,
-       label_type || case
-                         when balance >= 100
-                             and balance < 1000 then '4a'
-                         when balance >= 1000
-                             and balance < 10000 then '4b'
-                         when balance >= 10000
-                             and balance < 50000 then '4c'
-                         when balance >= 50000
-                             and balance < 100000 then '4d'
-                         when balance >= 100000
-                             and balance < 500000 then '4e'
-                         when balance >= 500000
-                             then '4f'
-           end               as label_name,
-       balance               as data,
-       'NFT'                 as wired_type,
-       now()                 as updated_at,
-       'b'                   as "group",
-       case
-           when balance >= 100
-               and balance < 1000 then 'L1'
-           when balance >= 1000
-               and balance < 10000 then 'L2'
-           when balance >= 10000
-               and balance < 50000 then 'L3'
-           when balance >= 50000
-               and balance < 100000 then 'L4'
-           when balance >= 100000
-               and balance < 500000 then 'L5'
-           when balance >= 500000
-               then 'L6' end as level,
-       'grade'               as category,
-       t.type                as trade_type,
-       ''                    as project,
-       t.token_name          as asset,
-       'balance'             as bus_type
-from (
-         -- project(null)+nft+type(null)
-         select a1.address,
-                a2.label_type,
-                a2.token_name,
-                a2.project_name,
-                a2.type,
-                sum(balance_usd) as balance
-         from nft_balance_usd_temp a1
-                  inner join dim_project_token_type_temp a2
-                             on a1.token = a2.token and a2.project = '' and (a2.type = '' OR a2.type = 'ALL') and
-                                a2.data_subject = 'balance_grade' and a2.wired_type='NFT'
-         group by a1.address,
-                  a2.label_type,
-                  a2.token_name,
-                  a2.project_name,
-                  a2.type
-         union all
-         -- project(null)+nft(ALL)+type(null)
-         select a1.address,
-                a2.label_type,
-                a2.token_name,
-                a2.project_name,
-                a2.type,
-                sum(balance_usd) as balance
-         from nft_balance_usd_temp a1
-                  inner join dim_project_token_type_temp a2
-                             on a2.token = 'ALL' and a2.project = '' and (a2.type = '' OR a2.type = 'ALL') and
-                                a2.data_subject = 'balance_grade' and a2.wired_type='NFT'
-         group by a1.address,
-                  a2.label_type,
-                  a2.token_name,
-                  a2.project_name,
-                  a2.type) t
-where balance >= 100
-  and address not in (select address from exclude_address);
-insert into tag_result(table_name, batch_date)
-SELECT 'address_label_nft_balance_usd_grade' as table_name, '${batchDate}' as batch_date;
+-- drop table if exists address_label_nft_balance_usd_grade;
+-- CREATE TABLE public.address_label_nft_balance_usd_grade
+-- (
+--     address          varchar(512) NULL,
+--     data             numeric(250, 20) NULL,
+--     wired_type       varchar(20) NULL,
+--     label_type       varchar(512) NULL,
+--     label_name       varchar(1024) NULL,
+--     updated_at       timestamp(6) NULL,
+--     "group"          varchar(1) NULL,
+--     "level"          varchar(100) NULL,
+--     category         varchar(100) NULL,
+--     trade_type       varchar(100) NULL,
+--     project          varchar(100) NULL,
+--     asset            varchar(100) NULL,
+--     bus_type         varchar(20) NULL,
+--     recent_time_code varchar(30) NULL
+-- ) with (appendonly = 'true', compresstype = zstd, compresslevel = '5')
+--     distributed by
+-- (
+--     address,
+--     label_name,
+--     recent_time_code
+-- );
+-- truncate table public.address_label_nft_balance_usd_grade;
+-- vacuum
+-- address_label_nft_balance_usd_grade;
+--
+-- insert into public.address_label_nft_balance_usd_grade(address, label_type, label_name, data, wired_type, updated_at,
+--                                                        "group", level, category, trade_type, project, asset, bus_type)
+-- select address
+--         ,
+--        label_type
+--         ,
+--        label_type || case
+--                          when balance >= 100
+--                              and balance < 1000 then '4a'
+--                          when balance >= 1000
+--                              and balance < 10000 then '4b'
+--                          when balance >= 10000
+--                              and balance < 50000 then '4c'
+--                          when balance >= 50000
+--                              and balance < 100000 then '4d'
+--                          when balance >= 100000
+--                              and balance < 500000 then '4e'
+--                          when balance >= 500000
+--                              then '4f'
+--            end               as label_name,
+--        balance               as data,
+--        'NFT'                 as wired_type,
+--        now()                 as updated_at,
+--        'b'                   as "group",
+--        case
+--            when balance >= 100
+--                and balance < 1000 then 'L1'
+--            when balance >= 1000
+--                and balance < 10000 then 'L2'
+--            when balance >= 10000
+--                and balance < 50000 then 'L3'
+--            when balance >= 50000
+--                and balance < 100000 then 'L4'
+--            when balance >= 100000
+--                and balance < 500000 then 'L5'
+--            when balance >= 500000
+--                then 'L6' end as level,
+--        'grade'               as category,
+--        t.type                as trade_type,
+--        ''                    as project,
+--        t.token_name          as asset,
+--        'balance'             as bus_type
+-- from (
+--          -- project(null)+nft+type(null)
+--          select a1.address,
+--                 a2.label_type,
+--                 a2.token_name,
+--                 a2.project_name,
+--                 a2.type,
+--                 sum(balance_usd) as balance
+--          from nft_balance_usd_temp a1
+--                   inner join dim_project_token_type_temp a2
+--                              on a1.token = a2.token and a2.project = '' and (a2.type = '' OR a2.type = 'ALL') and
+--                                 a2.data_subject = 'balance_grade' and a2.wired_type='NFT'
+--          group by a1.address,
+--                   a2.label_type,
+--                   a2.token_name,
+--                   a2.project_name,
+--                   a2.type
+--          union all
+--          -- project(null)+nft(ALL)+type(null)
+--          select a1.address,
+--                 a2.label_type,
+--                 a2.token_name,
+--                 a2.project_name,
+--                 a2.type,
+--                 sum(balance_usd) as balance
+--          from nft_balance_usd_temp a1
+--                   inner join dim_project_token_type_temp a2
+--                              on a2.token = 'ALL' and a2.project = '' and (a2.type = '' OR a2.type = 'ALL') and
+--                                 a2.data_subject = 'balance_grade' and a2.wired_type='NFT'
+--          group by a1.address,
+--                   a2.label_type,
+--                   a2.token_name,
+--                   a2.project_name,
+--                   a2.type) t
+-- where balance >= 100
+--   and address not in (select address from exclude_address);
+-- insert into tag_result(table_name, batch_date)
+-- SELECT 'address_label_nft_balance_usd_grade' as table_name, '${batchDate}' as batch_date;
--- app/tag-app/src/main/resources/tagscripts/address_label_nft_balance_usd_top.sql
+++ app/tag-app/src/main/resources/tagscripts/address_label_nft_balance_usd_top.sql
@@ -1,94 +1,94 @@
-drop table if exists address_label_nft_balance_usd_top;
-CREATE TABLE public.address_label_nft_balance_usd_top
-(
-    address          varchar(512) NULL,
-    data             numeric(250, 20) NULL,
-    wired_type       varchar(20) NULL,
-    label_type       varchar(512) NULL,
-    label_name       varchar(1024) NULL,
-    updated_at       timestamp(6) NULL,
-    "group"          varchar(1) NULL,
-    "level"          varchar(100) NULL,
-    category         varchar(100) NULL,
-    trade_type       varchar(100) NULL,
-    project          varchar(100) NULL,
-    asset            varchar(100) NULL,
-    bus_type         varchar(20) NULL,
-    recent_time_code varchar(30) NULL
-) with (appendonly = 'true', compresstype = zstd, compresslevel = '5')
-    distributed by
-(
-    address,
-    label_name,
-    recent_time_code
-);
-truncate table address_label_nft_balance_usd_top;
-vacuum
-address_label_nft_balance_usd_top;
-
-insert into public.address_label_nft_balance_usd_top(address, label_type, label_name, data, wired_type, updated_at,
-                                                     "group", level, category, trade_type, project, asset, bus_type)
-select address,
-       label_type,
-       label_type || '4k' as label_name,
-       rn                 as data,
-       'NFT'              as wired_type,
-       now()              as updated_at,
-       'b'                as "group",
-       'WHALE'            as level,
-       'top'              as category,
-       t.type             as trade_type,
-       ''                 as project,
-       t.token_name       as asset,
-       'balance'          as bus_type
-from (select address,
-             dptt.label_type   as label_type,
-             dptt.type         as type,
-             dptt.project_name as project_name,
-             dptt.token_name   as token_name,
-             s1.rn
-      from (select a1.address,
-                   seq_flag,
-                   -- 分组字段很关键
-                   row_number() over( partition by seq_flag
-		order by
-			balance desc,
-			address asc) as rn
-            from (select address,
-                         seq_flag,
-                         sum(balance) as balance
-                  from (select address,
-                               token,
-                               balance_usd as balance
-                        from nft_balance_usd_temp
-                        where address not in (select address from exclude_address)
-                          and token in (select token_id from dim_project_token_type_rank_temp dpttr)
-                        union all
-                        select address,
-                               'ALL'       as token,
-                               balance_usd as balance
-                        from nft_balance_usd_temp
-                        where address not in (select address from exclude_address)
-                          and token in (select token_id from dim_project_token_type_rank_temp dpttr)) totala
-                           inner join dim_project_token_type_temp a2
-                                      on
-                                                  totala.token = a2.token
-                                              and a2.data_subject = 'balance_top'
-                                             and a2.wired_type='NFT'
-                                              and a2.project = ''
-                                              and (a2.type = ''
-                                              or a2.type = 'ALL')
-                  where totala.balance >= 100
-                  group by address,
-                           seq_flag) a1) s1
-               inner join dim_project_token_type_temp dptt on (
-                  dptt.seq_flag = s1.seq_flag
-              and dptt.data_subject = 'balance_top'
-             and dptt.wired_type='NFT'
-              and dptt.project = ''
-              and (dptt.type = ''
-              or dptt.type = 'ALL')
-          )
-      where s1.rn <= 100) t;
-insert into tag_result(table_name, batch_date)
-SELECT 'address_label_nft_balance_usd_top' as table_name, '${batchDate}' as batch_date;
+-- drop table if exists address_label_nft_balance_usd_top;
+-- CREATE TABLE public.address_label_nft_balance_usd_top
+-- (
+--     address          varchar(512) NULL,
+--     data             numeric(250, 20) NULL,
+--     wired_type       varchar(20) NULL,
+--     label_type       varchar(512) NULL,
+--     label_name       varchar(1024) NULL,
+--     updated_at       timestamp(6) NULL,
+--     "group"          varchar(1) NULL,
+--     "level"          varchar(100) NULL,
+--     category         varchar(100) NULL,
+--     trade_type       varchar(100) NULL,
+--     project          varchar(100) NULL,
+--     asset            varchar(100) NULL,
+--     bus_type         varchar(20) NULL,
+--     recent_time_code varchar(30) NULL
+-- ) with (appendonly = 'true', compresstype = zstd, compresslevel = '5')
+--     distributed by
+-- (
+--     address,
+--     label_name,
+--     recent_time_code
+-- );
+-- truncate table address_label_nft_balance_usd_top;
+-- vacuum
+-- address_label_nft_balance_usd_top;
+--
+-- insert into public.address_label_nft_balance_usd_top(address, label_type, label_name, data, wired_type, updated_at,
+--                                                      "group", level, category, trade_type, project, asset, bus_type)
+-- select address,
+--        label_type,
+--        label_type || '4k' as label_name,
+--        rn                 as data,
+--        'NFT'              as wired_type,
+--        now()              as updated_at,
+--        'b'                as "group",
+--        'WHALE'            as level,
+--        'top'              as category,
+--        t.type             as trade_type,
+--        ''                 as project,
+--        t.token_name       as asset,
+--        'balance'          as bus_type
+-- from (select address,
+--              dptt.label_type   as label_type,
+--              dptt.type         as type,
+--              dptt.project_name as project_name,
+--              dptt.token_name   as token_name,
+--              s1.rn
+--       from (select a1.address,
+--                    seq_flag,
+--                    -- 分组字段很关键
+--                    row_number() over( partition by seq_flag
+-- 		order by
+-- 			balance desc,
+-- 			address asc) as rn
+--             from (select address,
+--                          seq_flag,
+--                          sum(balance) as balance
+--                   from (select address,
+--                                token,
+--                                balance_usd as balance
+--                         from nft_balance_usd_temp
+--                         where address not in (select address from exclude_address)
+--                           and token in (select token_id from dim_project_token_type_rank_temp dpttr)
+--                         union all
+--                         select address,
+--                                'ALL'       as token,
+--                                balance_usd as balance
+--                         from nft_balance_usd_temp
+--                         where address not in (select address from exclude_address)
+--                           and token in (select token_id from dim_project_token_type_rank_temp dpttr)) totala
+--                            inner join dim_project_token_type_temp a2
+--                                       on
+--                                                   totala.token = a2.token
+--                                               and a2.data_subject = 'balance_top'
+--                                              and a2.wired_type='NFT'
+--                                               and a2.project = ''
+--                                               and (a2.type = ''
+--                                               or a2.type = 'ALL')
+--                   where totala.balance >= 100
+--                   group by address,
+--                            seq_flag) a1) s1
+--                inner join dim_project_token_type_temp dptt on (
+--                   dptt.seq_flag = s1.seq_flag
+--               and dptt.data_subject = 'balance_top'
+--              and dptt.wired_type='NFT'
+--               and dptt.project = ''
+--               and (dptt.type = ''
+--               or dptt.type = 'ALL')
+--           )
+--       where s1.rn <= 100) t;
+-- insert into tag_result(table_name, batch_date)
+-- SELECT 'address_label_nft_balance_usd_top' as table_name, '${batchDate}' as batch_date;
