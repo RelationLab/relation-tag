9b5da944e2ddf953bd15a30e1a703951228e7c25
HP <panzuchao@126.com>
2023-08-24 00:06:51 +0800

    and recent_time_code ='ALL'

--- app/tag-app/src/main/resources/tagscripts/address_label_nft_project_type_count_grade.sql
+++ app/tag-app/src/main/resources/tagscripts/address_label_nft_project_type_count_grade.sql
@@ -195,6 +195,6 @@ from (
              a2.token_name,
              recent_time_code) t
 where sum_count >= 1
-  and address not in (select address from exclude_address)
+  and address not in (select address from exclude_address);
 insert into tag_result(table_name, batch_date)
 SELECT 'address_label_nft_project_type_count_grade' as table_name, '${batchDate}' as batch_date;
