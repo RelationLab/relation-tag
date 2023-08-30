f5b1f3fbcfac8ac7334fa4b2d22f0094c5be772e
HP <panzuchao@126.com>
2023-08-30 20:56:44 +0800

    asstatistical_type直接赋值成nft拉

--- app/tag-app/src/main/resources/tag-summary-init-scripts/nft_holding_summary.sql
+++ app/tag-app/src/main/resources/tag-summary-init-scripts/nft_holding_summary.sql
@@ -1,19 +1,19 @@
 insert
 into nft_holding_temp(address,
-                 token,
-                 balance,
-                 total_transfer_volume,
-                 total_transfer_count,
-                 total_transfer_to_volume,
-                 total_transfer_to_count,
-                 total_transfer_mint_volume,
-                 total_transfer_mint_count,
-                 total_transfer_burn_volume,
-                 total_transfer_burn_count,
-                 total_transfer_all_volume,
-                 total_transfer_all_count,
-                 updated_block_height,
-                 recent_time_code)
+                      token,
+                      balance,
+                      total_transfer_volume,
+                      total_transfer_count,
+                      total_transfer_to_volume,
+                      total_transfer_to_count,
+                      total_transfer_mint_volume,
+                      total_transfer_mint_count,
+                      total_transfer_burn_volume,
+                      total_transfer_burn_count,
+                      total_transfer_all_volume,
+                      total_transfer_all_count,
+                      updated_block_height,
+                      recent_time_code)
 
 select address,
        token,
@@ -31,6 +31,9 @@ select address,
        max(updated_block_height),
        recent_time_code
 from nft_holding_middle
+         inner join nft_sync_address on
+    (nft_sync_address.address = nft_holding_middle.token)
+WHERE nft_sync_address.type <> 'ERC1155'
 group by address, token, recent_time_code;
 insert into tag_result(table_name, batch_date)
 SELECT 'nft_holding_summary' as table_name, '${batchDate}' as batch_date;
