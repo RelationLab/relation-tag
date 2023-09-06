ac168fbc12e3324b1ad60ebce78b12731b3c4f40
HP <panzuchao@126.com>
2023-09-06 18:54:43 +0800

    NFT交易类型标签丢失

--- app/tag-app/src/main/resources/basic-data-scripts/basic_data_platform_large_category.sql
+++ app/tag-app/src/main/resources/basic-data-scripts/basic_data_platform_large_category.sql
@@ -10,7 +10,7 @@ insert into platform_large_category(code,platform_large_code,platform_large_name
 insert into platform_large_category(code,platform_large_code,platform_large_name) values('d2','ETH2.0','Eth2.0');
 insert into platform_large_category(code,platform_large_code,platform_large_name) values('d3','LENDING','Lending');
 insert into platform_large_category(code,platform_large_code,platform_large_name) values('d4','SERVICES','Services');
-insert into platform_large_category(code,platform_large_code,platform_large_name) values('d4','DERIVATIVES','Derivatives');
+insert into platform_large_category(code,platform_large_code,platform_large_name) values('d5','DERIVATIVES','Derivatives');
 
 insert into tag_result(table_name,batch_date)  SELECT 'basic_data_platform_large_category' as table_name,'${batchDate}'  as batch_date;
 
