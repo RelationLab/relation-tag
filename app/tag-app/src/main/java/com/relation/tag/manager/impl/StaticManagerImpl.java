62ef0d7c690c0ebc9c249eb9a25c4c658b479a2e
HP <panzuchao@126.com>
2023-09-15 20:05:41 +0800

    dex更改和维度表时间维度

--- app/tag-app/src/main/java/com/relation/tag/manager/impl/StaticManagerImpl.java
+++ app/tag-app/src/main/java/com/relation/tag/manager/impl/StaticManagerImpl.java
@@ -436,9 +436,9 @@ public class StaticManagerImpl implements StaticManager {
     private String buildConditionDataByLabel(String labels, UgcLabelDataAnalysis entity) {
         String tatolStartSql = "select distinct address as address from (";
         String outSql = "select address from (";
-        String systemSql = "(recent_time_code='ALL' or recent_time_code is null ) AND ";
+//        String systemSql = "(recent_time_code='ALL' or recent_time_code is null ) AND ";
         String selectStartSql = " select address from address_label_gp_" + configEnvironment +
-                " where  "+systemSql +" label_name ='";
+                " where  " +" label_name ='";
         String selectEndSql = "' ";
         String fuzzyKey = " UNION ";
         String precisionKey = " INTERSECT ";
@@ -454,7 +454,7 @@ public class StaticManagerImpl implements StaticManager {
             }
         }
         String addressLabelGpStr = outSql.concat(stringBuffer.toString()).concat(") total_in ");
-        String addressLabelUgcStr = addressLabelGpStr.replace("address_label_gp", "address_label_ugc").replace(systemSql,"");
+        String addressLabelUgcStr = addressLabelGpStr.replace("address_label_gp", "address_label_ugc");
         String tatolEndSql = ") total_out";
         String execSql = tatolStartSql.concat(addressLabelGpStr).concat(fuzzyKey)
                 .concat(addressLabelUgcStr).concat(tatolEndSql);
