28e82b9324047f842737ad143755e0ad409a0bbe
HP <panzuchao@126.com>
2023-09-21 14:19:32 +0800

    dex更改和维度表时间维度

--- app/tag-app/src/main/java/com/relation/tag/TagApplication.java
+++ app/tag-app/src/main/java/com/relation/tag/TagApplication.java
@@ -1,16 +1,9 @@
 package com.relation.tag;
 
-import com.relation.tag.manager.TagAddressManager;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.http.client.utils.DateUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.util.Calendar;
-import java.util.Date;
 
 @SpringBootApplication(scanBasePackages = {"com.relation.tag",
         "org.springframework.boot.extension"})
@@ -19,42 +12,43 @@ import java.util.Date;
 public class TagApplication {
 
     public static void main(String[] args) throws Exception {
-        ConfigurableApplicationContext ctx = SpringApplication.run(TagApplication.class, args);
-        String configEnvironment = ctx.getEnvironment().getProperty("config.environment");
-        String tagFlag = ctx.getEnvironment().getProperty("config.tag.flag");
-        if (StringUtils.equals(configEnvironment,"dev")){
-            return;
-        }
-        if (!StringUtils.equals(tagFlag,"tag")){
-            return;
-        }
-        log.info("configEnvironment====={}",configEnvironment);
-        configEnvironment = StringUtils.isEmpty(configEnvironment)?"stag":configEnvironment;
-        TagAddressManager tagAddressManager = ctx.getBean(TagAddressManager.class);
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(new Date());
-        calendar.set(Calendar.HOUR,calendar.get(Calendar.HOUR) + 8);
-        String batchDate = DateUtils.formatDate(calendar.getTime(), "YYYY-MM-dd");
-        String checkTable = "rename_table_".concat(configEnvironment);
-        if (tagAddressManager.checkResult(checkTable, batchDate, 1, false)){
-            log.info("checkResult tag end...........");
-            return;
-//            System.exit(0);
-        }
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    tagAddressManager.refreshAllLabel(batchDate);
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }).start();
-        Thread.sleep(60*60*1000);
-        log.info("check address_labels_json_gin start...........");
-        tagAddressManager.check(checkTable, 1 * 60 * 1000, batchDate, 1, false);
-        log.info("tag end...........");
+//        ConfigurableApplicationContext ctx =
+        SpringApplication.run(TagApplication.class, args);
+//        String configEnvironment = ctx.getEnvironment().getProperty("config.environment");
+//        String tagFlag = ctx.getEnvironment().getProperty("config.tag.flag");
+//        if (StringUtils.equals(configEnvironment,"dev")){
+//            return;
+//        }
+//        if (!StringUtils.equals(tagFlag,"tag")){
+//            return;
+//        }
+//        log.info("configEnvironment====={}",configEnvironment);
+//        configEnvironment = StringUtils.isEmpty(configEnvironment)?"stag":configEnvironment;
+//        TagAddressManager tagAddressManager = ctx.getBean(TagAddressManager.class);
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.setTime(new Date());
+//        calendar.set(Calendar.HOUR,calendar.get(Calendar.HOUR) + 8);
+//        String batchDate = DateUtils.formatDate(calendar.getTime(), "YYYY-MM-dd");
+//        String checkTable = "rename_table_".concat(configEnvironment);
+//        if (tagAddressManager.checkResult(checkTable, batchDate, 1, false)){
+//            log.info("checkResult tag end...........");
+//            return;
+////            System.exit(0);
+//        }
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    tagAddressManager.refreshAllLabel(batchDate);
+//                } catch (Exception e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }
+//        }).start();
+//        Thread.sleep(60*60*1000);
+//        log.info("check address_labels_json_gin start...........");
+//        tagAddressManager.check(checkTable, 1 * 60 * 1000, batchDate, 1, false);
+//        log.info("tag end...........");
 //        System.exit(0);
     }
 }
---
+++ app/tag-app/src/main/java/com/relation/tag/job/TagJob.java
@@ -0,0 +1,89 @@
+package com.relation.tag.job;
+
+import com.relation.tag.manager.TagAddressManager;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.client.utils.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Calendar;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class TagJob {
+
+    @Value("${config.environment}")
+    private String configEnvironment;
+    @Value("${config.tag.flag}")
+    private String tagFlag;
+
+    @Autowired
+    private TagAddressManager tagAddressManager;
+
+    /**
+     * 打测试环境
+     * @throws Exception
+     */
+    @Scheduled(cron = "0 0 19 * * ?")
+    public void execTagForStag() throws Exception {
+        if (!StringUtils.equals(configEnvironment, "stag")) {
+            return;
+        }
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.set(Calendar.HOUR, calendar.get(Calendar.HOUR) + 8);
+        String batchDate = DateUtils.formatDate(calendar.getTime(), "YYYY-MM-dd");
+        tag(batchDate);
+    }
+
+    /**
+     * 打生产环境
+     * @throws Exception
+     */
+    @Scheduled(cron = "0 0 1 * * ?")
+    public void execTagForProd() throws Exception {
+        if (!StringUtils.equals(configEnvironment, "prod")) {
+            return;
+        }
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.set(Calendar.HOUR, calendar.get(Calendar.HOUR) + 8);
+        // 将日期减一天
+        calendar.add(Calendar.DAY_OF_MONTH, -1);
+        String batchDate = DateUtils.formatDate(calendar.getTime(), "YYYY-MM-dd");
+        tag(batchDate);
+    }
+
+    private void tag(String batchDate) throws Exception {
+        if (!StringUtils.equals(tagFlag, "tag")) {
+            return;
+        }
+        log.info("configEnvironment====={}", configEnvironment);
+        configEnvironment = StringUtils.isEmpty(configEnvironment) ? "stag" : configEnvironment;
+
+        String checkTable = "rename_table_".concat(configEnvironment);
+        if (tagAddressManager.checkResult(checkTable, batchDate, 1, false)) {
+            log.info("checkResult tag end...........");
+            return;
+//            System.exit(0);
+        }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    tagAddressManager.refreshAllLabel(batchDate);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+        Thread.sleep(60 * 60 * 1000);
+        log.info("check address_labels_json_gin start...........");
+        tagAddressManager.check(checkTable, 1 * 60 * 1000, batchDate, 1, false);
+        log.info("tag end...........");
+    }
+}
